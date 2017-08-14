b26146d208a1450153d690b0eda218417d2b668d
Chen-Pang He <chen.pang.he@jdh8.org>
2017-08-15 03:13:10 +0800

    Add memcpy
    Although the code is naive, compiler will optimize because we have restrict.

---
+++ src/string.c
@@ -0,0 +1,17 @@
+#include <stddef.h>
+
+void* memcpy(void* restrict destination, const void* restrict source, size_t length)
+{
+    char* restrict dst = destination;
+    const char* restrict src = source;
+
+    /* I know this is naive, but compiler will optimized this because we have restrict. */
+    for (; length; --length)
+    {
+        *dst = *src;
+        ++dst;
+        ++src;
+    }
+
+    return destination;
+}
