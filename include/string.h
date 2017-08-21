bd689f5f7b5aa28f9307a6b4e39781e64617aa1d
Chen-Pang He <jdh8@ms63.hinet.net>
2017-08-21 17:21:24 +0800

    Get standard and safe prototype of ::memchr in C++

--- include/string.h
+++ include/string.h
@@ -2,12 +2,27 @@
 extern "C" {
 #endif
 
-void* memchr(const void* source, int c, size_t length);
 int memcmp(const void* lhs, const void* rhs, size_t length);
 void* memcpy(void*__restrict destination, const void*__restrict source, size_t length);
 void* memmove(void* destination, const void* source, size_t length);
 void* memset(void* destination, int c, size_t length);
 
 #ifdef __cplusplus
-}
+namespace __metallic {
+#endif
+
+void* memchr(const void* source, int c, size_t length);
+
+#ifdef __cplusplus
+} // namespace __metallic
+
+extern "C++" inline
+const void* memchr(const void* source, int c, size_t length)
+{ return __metallic::memchr(source, c, length); }
+
+extern "C++" inline
+void* memchr(void* source, int c, size_t length)
+{ return __metallic::memchr(source, c, length); }
+
+} // extern "C"
 #endif
