63cc7192f7f3e309466809b9533d8c32a5c1b5e2
Chen-Pang He <jdh8@ms63.hinet.net>
2017-08-24 00:13:34 +0800

    Start to polyfill long double

---
+++ src/arch/wasm/long-double.cpp
@@ -0,0 +1,76 @@
+#ifdef __wasm__
+
+#include <type_traits>
+#include <cstdint>
+
+namespace {
+
+struct Tetra
+{
+    [[gnu::mode(TF)]] typedef float Real;
+
+    unsigned __int128 mantissa: 112;
+    unsigned int exp : 15;
+    unsigned int sign:  1;
+
+    Tetra(Real);
+
+    explicit operator Real() const;
+    explicit operator __int128() const;
+    explicit operator bool() const;
+
+    bool isnan() const;
+};
+
+Tetra::Tetra(Real real)
+{
+    static_assert(sizeof(Tetra) == sizeof(Real), "Bug: [[gnu::mode(TF)]] float does not fit Tetra.");
+    __builtin_memcpy(this, &real, sizeof(Tetra));
+}
+
+Tetra::operator Real() const
+{
+    Real result;
+    __builtin_memcpy(&result, this, sizeof(Tetra));
+    return result;
+}
+
+Tetra::operator __int128() const
+{
+    __int128 result;
+    __builtin_memcpy(&result, this, sizeof(Tetra));
+    return result;
+}
+
+Tetra::operator bool() const
+{
+    return exp || mantissa;
+}
+
+bool Tetra::isnan() const
+{
+    return exp == 0x7fff && mantissa;
+}
+
+} // namespace
+
+bool operator==(Tetra a, Tetra b)
+{
+    return a.exp != 0x7fff && b.exp != 0x7fff && (!(a || b) || __int128(a) == __int128(b));
+}
+
+extern "C" {
+
+int __unordtf2(Tetra::Real a, Tetra::Real b)
+{
+    return Tetra(a).isnan() || Tetra(b).isnan();
+}
+
+int __eqtf2(Tetra::Real a, Tetra::Real b)
+{
+    return Tetra(a) == Tetra(b);
+}
+
+} // extern "C"
+
+#endif // __wasm__
