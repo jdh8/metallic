0c9b15ecdf2004ad754230e2a3b5145286685151
Chen-Pang He <jdh8@ms63.hinet.net>
2017-08-26 23:44:02 +0800

    Modularize libgcc-compatible low-level functions
    Revert to traditional C modularization because clang never inlines them anyway.
    Files in src/math will be replaced.

---
+++ src/soft/integer/ashrti3.c
@@ -0,0 +1,20 @@
+/* This file is part of Metallic, a C++ library for WebAssembly.
+ *
+ * Copyright (C) 2017 Chen-Pang He <chen.pang.he@jdh8.org>
+ *
+ * This Source Code Form is subject to the terms of the Mozilla
+ * Public License v. 2.0. If a copy of the MPL was not distributed
+ * with this file, You can obtain one at http://mozilla.org/MPL/2.0/
+ */
+#include <stdint.h>
+
+__int128 __ashrti3(__int128 integer, int shift)
+{
+    int64_t high = integer >> 64;
+    uint64_t low = integer;
+
+    if (shift & 64)
+        return high >> shift;
+    else
+        return (__int128)(high >> shift) << 64 | (high << -shift | low >> shift);
+}
---
+++ src/soft/integer/lshrti3.c
@@ -0,0 +1,20 @@
+/* This file is part of Metallic, a C++ library for WebAssembly.
+ *
+ * Copyright (C) 2017 Chen-Pang He <chen.pang.he@jdh8.org>
+ *
+ * This Source Code Form is subject to the terms of the Mozilla
+ * Public License v. 2.0. If a copy of the MPL was not distributed
+ * with this file, You can obtain one at http://mozilla.org/MPL/2.0/
+ */
+#include <stdint.h>
+
+unsigned __int128 __lshrti3(unsigned __int128 integer, int shift)
+{
+    uint64_t high = integer >> 64;
+    uint64_t low = integer;
+
+    if (shift & 64)
+        return high >> shift;
+    else
+        return (unsigned __int128)(high >> shift) << 64 | (high << -shift | low >> shift);
+}
