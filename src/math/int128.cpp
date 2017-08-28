b86851f69f3227f79c8aad51cd384680684f00c3
Chen-Pang He <jdh8@ms63.hinet.net>
2017-08-28 17:54:12 +0800

    Remove deprecated soft arithmetic routines

--- src/math/int128.cpp
+++
@@ -1,9 +0,0 @@
-#include "int128.hpp"
-
-extern "C" {
-
-__int128 __ashlti3(__int128 a, int s) { return Integer<__int128>(a) << s; }
-__int128 __ashrti3(__int128 a, int s) { return Integer<__int128>(a) >> s; }
-__int128 __lshrti3(__int128 a, int s) { return Integer<unsigned __int128>(a) >> s; }
-
-} // extern "C"
