3411a93ec9a3e3d64291b52190b58cbd9818c21d
Chen-Pang He <jdh8@ms63.hinet.net>
2017-08-29 17:55:26 +0800

    Incorporate fullmul to multi3 again because no other module uses it

--- src/soft/integer/multi3.c
+++ src/soft/integer/multi3.c
@@ -6,7 +6,23 @@
  * Public License v. 2.0. If a copy of the MPL was not distributed
  * with this file, You can obtain one at http://mozilla.org/MPL/2.0/
  */
-#include "fullmul.h"
+#include <stdint.h>
+
+static unsigned __int128 fullmul(uint64_t a, uint64_t b)
+{
+#ifdef __x86_64__
+    return (unsigned __int128) a * b;
+#else
+    uint32_t ahi = a >> 32;
+    uint32_t bhi = b >> 32;
+    uint32_t alo = a;
+    uint32_t blo = b;
+
+    return ((unsigned __int128) ahi * bhi << 64 | (uint64_t) alo * blo)
+        + ((unsigned __int128) ahi * blo << 32)
+        + ((unsigned __int128) alo * bhi << 32);
+#endif
+}
 
 unsigned __int128 __multi3(unsigned __int128 a, unsigned __int128 b)
 {
