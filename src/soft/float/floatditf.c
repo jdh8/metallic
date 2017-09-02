fcb2570a2a60611167dfb0c73f252834cb90ff4d
Chen-Pang He <jdh8@ms63.hinet.net>
2017-09-02 20:24:09 +0800

    Convert 64-bit integer to quadruple precision

---
+++ src/soft/float/floatditf.c
@@ -0,0 +1,25 @@
+/* This file is part of Metallic, a C++ library for WebAssembly.
+ *
+ * Copyright (C) 2017 Chen-Pang He <chen.pang.he@jdh8.org>
+ *
+ * This Source Code Form is subject to the terms of the Mozilla
+ * Public License v. 2.0. If a copy of the MPL was not distributed
+ * with this file, You can obtain one at http://mozilla.org/MPL/2.0/
+ */
+#include <float.h>
+#include <stdint.h>
+#include <stdlib.h>
+
+long double __floatditf(int64_t a)
+{
+    double x = a;
+    uint64_t source = *(uint64_t*)&x;
+
+    const int shift = (128 - LDBL_MANT_DIG) - (64 - DBL_MANT_DIG);
+    uint64_t bias = (0x3CLL << 56) * !!a;
+    uint64_t high = (source >> shift) + bias;
+    uint64_t low = llabs(a) << __builtin_clzll(llabs(a)) << 49;
+    uint64_t vector __attribute__((vector_size(sizeof(long double)))) = { low, high };
+
+    return *(long double*)&vector;
+}
