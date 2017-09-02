612f3cdfe608b1df1ad9b48601973f781d6fbec7
Chen-Pang He <jdh8@ms63.hinet.net>
2017-09-03 02:47:52 +0800

    Avoid typedef struct to improve robustness

--- src/soft/float/floatuntitf.h
+++ src/soft/float/floatuntitf.h
@@ -9,14 +9,13 @@
 #include <float.h>
 #include <stdint.h>
 
-typedef struct
+struct Normalized
 {
     unsigned __int128 integer;
     int clz;
-}
-Normalized;
+};
 
-static Normalized normalize(unsigned __int128 a)
+static struct Normalized normalize(unsigned __int128 a)
 {
     uint64_t high = a >> 64;
     uint64_t low = a;
@@ -24,14 +23,14 @@ static Normalized normalize(unsigned __int128 a)
     if (high) {
         int shift = __builtin_clzll(high);
         a = (unsigned __int128)(high << shift | low >> -shift) << 64 | low << shift;
-        return (Normalized) { a, shift };
+        return (struct Normalized) { a, shift };
     }
     if (low) {
         int shift = __builtin_clzll(low);
         a = (unsigned __int128)(low << shift) << 64;
-        return (Normalized) { a, shift|64 };
+        return (struct Normalized) { a, shift|64 };
     }
-    return (Normalized) { a, 128 };
+    return (struct Normalized) { a, 128 };
 }
 
 static long double floatuntitf(unsigned __int128 a)
@@ -39,7 +38,7 @@ static long double floatuntitf(unsigned __int128 a)
     const unsigned __int128 umax = -1;
     const unsigned __int128 smax = umax / 2;
 
-    Normalized normalized = normalize(a);
+    struct Normalized normalized = normalize(a);
     unsigned __int128 representation = (normalized.integer & smax) >> 15;
     int exp = 0x3FFF + 127 - normalized.clz;
 
