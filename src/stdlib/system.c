29271ae96e0c1f4d53a29568480ad30d149b6f99
Chen-Pang He <jdh8@ms63.hinet.net>
2017-09-05 12:49:07 +0800

    A simple implementation of system()

---
+++ src/stdlib/system.c
@@ -0,0 +1,14 @@
+/* This file is part of Metallic, a C++ library for WebAssembly.
+ *
+ * Copyright (C) 2017 Chen-Pang He <chen.pang.he@jdh8.org>
+ *
+ * This Source Code Form is subject to the terms of the Mozilla
+ * Public License v. 2.0. If a copy of the MPL was not distributed
+ * with this file, You can obtain one at http://mozilla.org/MPL/2.0/
+ */
+
+/* This function shall always fail because there is no shell.
+ * Queried with NULL, this function returns 0.
+ * With a command, this function returns EXIT_FAILURE.
+ */
+int system(const char* command) { return !!command; }
