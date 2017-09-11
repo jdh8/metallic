e3aeb63c13419277ec2428048d69f5ae530d4366
Chen-Pang He <chen.pang.he@jdh8.org>
2017-09-12 02:06:50 +0800

    Pseudorandom number generator

---
+++ libc/stdlib/rand.c
@@ -0,0 +1,23 @@
+/* This file is part of Metallic, a runtime library for WebAssembly.
+ *
+ * Copyright (C) 2017 Chen-Pang He <chen.pang.he@jdh8.org>
+ *
+ * This Source Code Form is subject to the terms of the Mozilla
+ * Public License v. 2.0. If a copy of the MPL was not distributed
+ * with this file, You can obtain one at http://mozilla.org/MPL/2.0/
+ */
+#include <stdint.h>
+
+static uint_fast32_t state = 1;
+
+void srand(unsigned seed)
+{
+    state = seed;
+}
+
+int rand(void)
+{
+    return state = state * 48271 % 2147483647;
+}
+
+int random(void) __attribute__((alias("rand"))); 
