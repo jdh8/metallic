9a5dbdb434c49e149e4f5a7f3648a4945068a953
Chen-Pang He <jdh8@ms63.hinet.net>
2017-09-18 21:27:37 +0800

    Fix std::is_constructible<void>

---
+++ test/type_traits/is_constructible.cpp
@@ -0,0 +1,11 @@
+// This file is part of Metallic, a runtime library for WebAssembly.
+//
+// Copyright (C) 2017 Chen-Pang He <chen.pang.he@jdh8.org>
+//
+// This Source Code Form is subject to the terms of the Mozilla
+// Public License v. 2.0. If a copy of the MPL was not distributed
+// with this file, You can obtain one at http://mozilla.org/MPL/2.0/
+
+#include <type_traits>
+
+static_assert(!std::is_constructible<void>::value, "False positive");
