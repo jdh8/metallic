9b25b2554463e3814db54f4450bb24638debf000
Chen-Pang He <jdh8@ms63.hinet.net>
2017-09-18 20:10:16 +0800

    Refactor tests

--- test/type_traits/is_integral.cpp
+++ test/type_traits/is_integral.cpp
@@ -1,9 +1,17 @@
+// This file is part of Metallic, a runtime library for WebAssembly.
+//
+// Copyright (C) 2017 Chen-Pang He <chen.pang.he@jdh8.org>
+//
+// This Source Code Form is subject to the terms of the Mozilla
+// Public License v. 2.0. If a copy of the MPL was not distributed
+// with this file, You can obtain one at http://mozilla.org/MPL/2.0/
+
 #include <type_traits>
 
-static_assert(std::is_integral<const bool>::value, "Type II error in std::is_integral");
-static_assert(std::is_integral<long>::value, "Type II error in std::is_integral");
-static_assert(std::is_integral<volatile unsigned>::value, "Type II error in std::is_integral");
+static_assert(std::is_integral<int>::value, "False negative");
+static_assert(std::is_integral<const bool>::value, "False negative");
+static_assert(std::is_integral<volatile unsigned>::value, "False negative");
 
-static_assert(!std::is_integral<float>::value, "Type I error in std::is_integral");
-static_assert(!std::is_integral<void>::value, "Type I error in std::is_integral");
-static_assert(!std::is_integral<void*>::value, "Type I error in std::is_integral");
+static_assert(!std::is_integral<float>::value, "False positive");
+static_assert(!std::is_integral<const void>::value, "False positive");
+static_assert(!std::is_integral<volatile void*>::value, "False positive");
--- test/type_traits/is_void.cpp
+++ test/type_traits/is_void.cpp
@@ -1,12 +1,20 @@
+// This file is part of Metallic, a runtime library for WebAssembly.
+//
+// Copyright (C) 2017 Chen-Pang He <chen.pang.he@jdh8.org>
+//
+// This Source Code Form is subject to the terms of the Mozilla
+// Public License v. 2.0. If a copy of the MPL was not distributed
+// with this file, You can obtain one at http://mozilla.org/MPL/2.0/
+
 #include <type_traits>
 
-static_assert(std::is_void<void>::value, "Type II error in std::is_void");
-static_assert(std::is_void<const void>::value, "Type II error in std::is_void");
-static_assert(std::is_void<volatile void>::value, "Type II error in std::is_void");
-static_assert(std::is_void<const volatile void>::value, "Type II error in std::is_void");
+static_assert(std::is_void<void>::value, "False negative");
+static_assert(std::is_void<const void>::value, "False negative");
+static_assert(std::is_void<volatile void>::value, "False negative");
+static_assert(std::is_void<const volatile void>::value, "False negative");
 
 class Dummy;
 
-static_assert(!std::is_void<const void*>::value, "Type I error in std::is_void");
-static_assert(!std::is_void<char>::value, "Type I error in std::is_void");
-static_assert(!std::is_void<Dummy>::value, "Type I error in std::is_void");
+static_assert(!std::is_void<const void*>::value, "False positive");
+static_assert(!std::is_void<char>::value, "False positive");
+static_assert(!std::is_void<Dummy>::value, "False positive");
