2610bcd0e84f339f88166475b5c6e9118039128b
Chen-Pang He <jdh8@ms63.hinet.net>
2017-09-27 20:27:29 +0800

    Remainder functions for float

---
+++ c/math/float/fmodf.c
@@ -0,0 +1,18 @@
+/* This file is part of Metallic, a runtime library for WebAssembly.
+ *
+ * Copyright (C) 2017 Chen-Pang He <chen.pang.he@jdh8.org>
+ *
+ * This Source Code Form is subject to the terms of the Mozilla
+ * Public License v. 2.0. If a copy of the MPL was not distributed
+ * with this file, You can obtain one at http://mozilla.org/MPL/2.0/
+ */
+#include <math.h>
+
+float fmodf(float numerator, float denominator)
+{
+    double a = numerator;
+    double b = denominator;
+    double q = trunc(a / b);
+
+    return a - b * q;
+}
