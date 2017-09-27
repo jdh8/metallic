6629cbb34849ee09512968c149ae5abb960e6dca
Chen-Pang He <chen.pang.he@jdh8.org>
2017-09-28 02:28:06 +0800

    Improve frexpf with shift

--- c/math/float/frexpf.c
+++ c/math/float/frexpf.c
@@ -6,28 +6,26 @@
  * Public License v. 2.0. If a copy of the MPL was not distributed
  * with this file, You can obtain one at http://mozilla.org/MPL/2.0/
  */
-#include <math.h>
-#include <limits.h>
 #include <stdint.h>
 
 float frexpf(float x, int* exp)
 {
     uint32_t word = *(uint32_t*)&x;
-    uint32_t wabs = word & (uint32_t)-1 / 2;
+    uint32_t wabs = word << 1;
 
-    if (wabs >= 0x7F800000 || wabs == 0) {
+    if (wabs >= 0xFF000000 || wabs == 0) {
         *exp = 0;
         return x;
     }
 
     int shift = __builtin_clz(wabs);
 
-    if (shift > 8) {
-        *exp = -(117 + shift);
-        word <<= (shift - 8);
+    if (shift > 7) {
+        *exp = -(118 + shift);
+        word <<= (shift - 7);
     }
     else {
-        *exp = (wabs >> 23) - 126;
+        *exp = (wabs >> 24) - 126;
     }
 
     word = (word & 0x807FFFFF) | 0x3F000000;
