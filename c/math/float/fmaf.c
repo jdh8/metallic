3f3d4bc0361c313f3fad80d3c6541ea88c019abf
Chen-Pang He <jdh8@ms63.hinet.net>
2017-09-27 20:29:44 +0800

    Implement fmaf

---
+++ c/math/float/fmaf.c
@@ -0,0 +1,12 @@
+/* This file is part of Metallic, a runtime library for WebAssembly.
+ *
+ * Copyright (C) 2017 Chen-Pang He <chen.pang.he@jdh8.org>
+ *
+ * This Source Code Form is subject to the terms of the Mozilla
+ * Public License v. 2.0. If a copy of the MPL was not distributed
+ * with this file, You can obtain one at http://mozilla.org/MPL/2.0/
+ */
+float fmaf(float x, float y, float a)
+{
+    return (double) x * y + a;
+}
