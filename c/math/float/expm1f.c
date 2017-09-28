2f2a8c6e66b3a707e4ddba4b88973207f1ee8dd9
Chen-Pang He <jdh8@ms63.hinet.net>
2017-09-29 00:03:05 +0800

    Make expm1f accurate

--- c/math/float/expm1f.c
+++ c/math/float/expm1f.c
@@ -7,23 +7,35 @@
  * with this file, You can obtain one at http://mozilla.org/MPL/2.0/
  */
 #include <math.h>
-
-static float __expm1f(float x)
-{
-    const int b[] = { 30240, 15120, 3360, 420, 30, 1 };
-
-    double a = x;
-    double a2 = a * a;
-    double a4 = a2 * a2;
-    double u = a * (b[5] * a4 + b[3] * a2 + b[1]);
-    double v = b[4] * a4 + b[2] * a2 + b[0];
-
-    return 2 * u / (v - u);
-}
+#include <stdint.h>
 
 float expm1f(float x)
 {
-    const float ln3 = 1.0986123085021973;
+    const float minimum = -103.972084045410;
+    const float maximum = 88.72283935546875;
 
-    return fabsf(x) <= ln3 ? __expm1f(x) : expf(x) - 1;
+    const float log2e = 1.44269502163;
+    const double ln2 = 0.6931471805599452862;
+    const int b[] = { 30240, 15120, 3360, 420, 30, 1 };
+
+    if (x < minimum)
+        return 0;
+
+    if (x > maximum)
+        return x * HUGE_VALF;
+
+    float n = nearbyintf(x * log2e);
+
+    double a = x - n * ln2;
+    double a2 = a * a;
+    double u = a * ((b[5] * a2 + b[3]) * a2 + b[1]);
+    double v = (b[4] * a2 + b[2]) * a2 + b[0];
+
+    if (n == 0)
+        return 2 * u / (v - u);
+
+    double y = (v + u) / (v - u);
+    int64_t shifted = *(int64_t*)&y + ((int64_t) n << 52);
+
+    return *(double*)&shifted - 1;
 }
