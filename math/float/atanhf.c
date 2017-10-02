c68968ca3f282a72cc8eb48788bfa099b32fedac
Chen-Pang He <jdh8@ms63.hinet.net>
2017-10-02 10:02:23 +0800

    Improve atanhf with double registers

--- math/float/atanhf.c
+++ math/float/atanhf.c
@@ -2,33 +2,17 @@
 #include "quietf.h"
 #include <math.h>
 
-static float _positive(float x)
+static double _finite(double x)
 {
-    const float ln2_2 = 0.34657359028;
-    const int32_t mantissa = 0x007FFFFF;
-    const int32_t sqrt2 = 0x3FB504F3;
+    const double ln2_2 = 0.3465735902799726547;
 
-    float y = (1 + x) / (1 - x);
-    int32_t word = *(int32_t*)&y;
-    int exponent = (word >> 23) - 127;
+    int exponent;
+    double y = logf_reduce((1 + x) / (1 - x), &exponent);
 
-    word = (word & mantissa) | 0x3F800000;
-
-    if (word >= sqrt2) {
-        word &= 0xFF7FFFFF;
-        ++exponent;
-    }
-
-    y = *(float*)&word;
-
-    switch (exponent) {
-        case 0:
-            return atanhf_taylor(x);
-        case 1:
-            return atanhf_taylor((float)(1 - 3.0 * x) / (x - 3)) + ln2_2;
-    }
-    
-    return atanhf_taylor((y - 1) / (y + 1)) + exponent * ln2_2;
+    if (exponent)
+        return atanhf_taylor((y - 1) / (y + 1)) + exponent * ln2_2;
+    else
+        return atanhf_taylor(x);
 }
 
 float atanhf(float x)
@@ -39,7 +23,7 @@ float atanhf(float x)
         return copysignf(HUGE_VALF, x);
 
     if (absx < 1)
-        return copysignf(_positive(absx), x);
+        return _finite(x);
 
     return quietf(x);
 }
