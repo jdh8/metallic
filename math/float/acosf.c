3eb7639f11908e737a7d3d345aa95dd120270636
Chen-Pang He <chen.pang.he@jdh8.org>
2017-10-05 02:00:11 +0800

    Fix incorrect results from acosf

--- math/float/acosf.c
+++ math/float/acosf.c
@@ -6,9 +6,18 @@
  * Public License v. 2.0. If a copy of the MPL was not distributed
  * with this file, You can obtain one at http://mozilla.org/MPL/2.0/
  */
-#include "asinfd.h"
+#include "atanf/octant.h"
+#include <math.h>
 
-float acosf(float x)
+static double asinfd(double x)
+{
+    double s = fabs(x);
+    double c = sqrt(1 - s * s);
+
+    return copysign(2 * atanf_octant(s / (1 + c)), x);
+}
+
+float f(float x)
 {
     const double pi_2 = 1.5707963267948966192;
 
