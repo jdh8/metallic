2b66faade9d0798b3056d390cdce747cce289a63
Chen-Pang He <jdh8@ms63.hinet.net>
2017-10-28 17:23:19 +0800

    Implement erfcf

--- math/float/erfcf.c
+++ math/float/erfcf.c
@@ -6,28 +6,11 @@
  * Public License v. 2.0. If a copy of the MPL was not distributed
  * with this file, You can obtain one at http://mozilla.org/MPL/2.0/
  */
-float kernel_erfcf(float x)
+#include "erfcf.h"
+
+float erfcf(float x)
 {
-    double c[] = {
-       -1.265512220104944281,
-        1.000022649889075839,
-        0.3741250376812044203,
-        0.09638813956533724532,
-       -0.1839662244217943646,
-        0.2713518068811310958,
-       -1.121100483072386768,
-        1.473256269115079037,
-       -0.8133262681389966881,
-        0.1687612120807513779
-    };
+    double t = right_erfcf(fabsf(x));
 
-    double t = 2 / (2 + x);
-    double t2 = t * t;
-    double t4 = t2 * t2;
-
-    double low = c[0] + c[1] * t + (c[2] + c[3] * t) * t2;
-    double mid = c[4] + c[5] * t + (c[6] + c[7] * t) * t2;
-    double hi = c[8] + c[9] * t;
-
-    return t * expfd(low + mid * t4 + hi * (t4 * t4) - x * x);
+    return signbit(x) ? 2 - t : t;
 }
