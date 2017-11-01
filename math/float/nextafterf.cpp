9b93fac2dde11cfac329d2b90a88c2cf8f98396d
Chen-Pang He <jdh8@ms63.hinet.net>
2017-11-01 15:59:16 +0800

    Rewrite nextafterf in C++

---
+++ math/float/nextafterf.cpp
@@ -0,0 +1,33 @@
+// This file is part of Metallic, a runtime library for WebAssembly.
+//
+// Copyright (C) 2017 Chen-Pang He <chen.pang.he@jdh8.org>
+//
+// This Source Code Form is subject to the terms of the Mozilla
+// Public License v. 2.0. If a copy of the MPL was not distributed
+// with this file, You can obtain one at http://mozilla.org/MPL/2.0/
+
+#include <internal/reinterpret>
+#include <cmath>
+#include <cstdint>
+
+extern "C"
+float nextafterf(float from, float to)
+{
+    using std::__internal::reinterpret;
+
+    if (from == to || to != to)
+        return to;
+
+    if (from != from)
+        return from;
+
+    if (from == 0)
+        return std::copysign(reinterpret<float>(1), to);
+
+    std::int32_t d = reinterpret<std::int32_t>(from);
+    std::int32_t a = reinterpret<std::int32_t>(to);
+
+    a < d || (a ^ d) < 0 ? --d : ++d;
+
+    return reinterpret<float>(d);
+}
