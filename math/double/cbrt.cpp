109b3f86fc8f0c3249a31e3c1e7c9c46dc1d17a8
Chen-Pang He <jdh8@ms63.hinet.net>
2017-11-05 23:56:11 +0800

    Implement cbrt(double)

---
+++ math/double/cbrt.cpp
@@ -0,0 +1,42 @@
+// This file is part of Metallic, a runtime library for WebAssembly.
+//
+// Copyright (C) 2017 Chen-Pang He <chen.pang.he@jdh8.org>
+//
+// This Source Code Form is subject to the terms of the Mozilla
+// Public License v. 2.0. If a copy of the MPL was not distributed
+// with this file, You can obtain one at http://mozilla.org/MPL/2.0/
+
+#include <internal/ieee754>
+#include <internal/reinterpret>
+#include <cmath>
+#include <cstdint>
+
+namespace std {
+namespace __internal {
+
+static double _kernel(double x)
+{
+    std::int64_t i = reinterpret<std::int64_t>(x);
+
+    if (i == 0 || i >= 0x7FF0000000000000)
+        return x;
+
+    i = 0x2A9F7AF196E8E6E8 + magnitude::normalize(i) / 3;
+
+    double y = reinterpret<double>(i);
+
+    y *= 0.5 + 1.5 * x / (2 * y * (y * y) + x);
+    y += 0.33333333333333333333 * (x / (y * y) - y);
+    y += 0.33333333333333333333 * (x / (y * y) - y);
+
+    return y;
+}
+
+extern "C"
+double cbrt(double x)
+{
+    return std::copysign(_kernel(std::abs(x)), x);
+}
+
+} // namespace __internal
+} // namespace std
