c5cf236f514992587217208318d8c47c03d13b2f
Chen-Pang He <jdh8@ms63.hinet.net>
2017-11-07 15:55:37 +0800

    New implementation of erff and erfcf

---
+++ math/float/erff.cpp
@@ -0,0 +1,41 @@
+// This file is part of Metallic, a runtime library for WebAssembly.
+//
+// Copyright (C) 2017 Chen-Pang He <chen.pang.he@jdh8.org>
+//
+// This Source Code Form is subject to the terms of the Mozilla
+// Public License v. 2.0. If a copy of the MPL was not distributed
+// with this file, You can obtain one at http://mozilla.org/MPL/2.0/
+
+#include "kernel/erfcf"
+#include <cmath>
+
+namespace std {
+namespace __internal {
+
+static double _kernel(double x)
+{
+    const double c[] = {
+        1.128379067473277685,
+       -0.3761122431043507220,
+        0.1125234763068763124,
+       -0.02461177835483573533
+    };
+
+    double xx = x * x;
+
+    return x * (c[0] + c[1] * xx + (c[2] + c[3] * xx) * (xx * xx));
+}
+
+static float _right(float x)
+{
+    return x > 0.4769362762044699f ? 1 - kernel::erfcf(x) : _kernel(x);
+}
+
+extern "C"
+float erff(float x)
+{
+    return std::copysign(_right(std::abs(x)), x);
+}
+
+} // namespace __internal
+} // namespace std
