1fae693c29e678b0983199c456d69955e5e54e7b
Chen-Pang He <jdh8@ms63.hinet.net>
2017-11-14 16:24:07 +0800

    Tidy up log2f

--- math/float/log2f.c
+++ math/float/log2f.c
@@ -13,37 +13,25 @@
 #include <float.h>
 #include <math.h>
 
-static float _finite(float x)
+static float _finite(int32_t i)
 {
     const double log2e = 1.44269504088896340736;
-    const int32_t sqrt1_2 = 0x3F3504F4;
 
-    int32_t magnitude = __normalizef(__bitsf(x));
-    int32_t exponent = (magnitude - sqrt1_2) >> (FLT_MANT_DIG - 1);
-    double y = __reinterpretf(magnitude - (exponent << (FLT_MANT_DIG - 1)));
+    int32_t exponent = (i - 0x3F3504F4) >> (FLT_MANT_DIG - 1);
+    double x = __reinterpretf(i - (exponent << (FLT_MANT_DIG - 1)));
 
-    return 2 * log2e * __kernel_atanhf((y - 1) / (y + 1)) + exponent;
+    return 2 * log2e * __kernel_atanhf((x - 1) / (x + 1)) + exponent;
 }
 
-float f(float x)
+float log2f(float x)
 {
-    const int32_t inf = 0x7F800000;
-
-    if (x == 0)
-        return -HUGE_VALF;
-
-    int32_t i = *(int32_t*)&x;
+    int32_t i = __bitsf(x);
 
     if (i < 0)
-        return __quietf(x);
+        return i << 1 == 0 ? -HUGE_VALF : __quietf(x);
 
-    if (i < inf)
-        return _finite(x);
+    if (i < 0x7F800000)
+        return _finite(__normalizef(i));
 
     return x;
-}
-
-float test(void)
-{
-    return f(8);
 }
