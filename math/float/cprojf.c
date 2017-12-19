11cc744ff615b30634c0680d7a2415c770d53dcf
Chen-Pang He <chen.pang.he@jdh8.org>
2017-12-20 01:39:21 +0800

    Implement cprojf

---
+++ math/float/cprojf.c
@@ -0,0 +1,18 @@
+/* This file is part of Metallic, a runtime library for WebAssembly.
+ *
+ * Copyright (C) 2017 Chen-Pang He <chen.pang.he@jdh8.org>
+ *
+ * This Source Code Form is subject to the terms of the Mozilla
+ * Public License v. 2.0. If a copy of the MPL was not distributed
+ * with this file, You can obtain one at http://mozilla.org/MPL/2.0/
+ */
+#include <complex.h>
+#include <math.h>
+
+float _Complex cprojf(float _Complex z)
+{
+    if (isinf((float)z) || isinf(cimagf(z)))
+        return HUGE_VALF;
+
+    return z;
+}
