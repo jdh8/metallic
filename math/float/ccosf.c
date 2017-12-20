9d665f217e469b4022e4aa1400ab90cc9f371275
Chen-Pang He <chen.pang.he@jdh8.org>
2017-12-20 11:38:54 +0800

    Implement ccoshf and ccosf

---
+++ math/float/ccosf.c
@@ -0,0 +1,14 @@
+/* This file is part of Metallic, a runtime library for WebAssembly.
+ *
+ * Copyright (C) 2017 Chen-Pang He <chen.pang.he@jdh8.org>
+ *
+ * This Source Code Form is subject to the terms of the Mozilla
+ * Public License v. 2.0. If a copy of the MPL was not distributed
+ * with this file, You can obtain one at http://mozilla.org/MPL/2.0/
+ */
+#include "ccoshf.h"
+
+float _Complex ccosf(float _Complex z)
+{
+    return __ccoshf(CMPLXF(-cimagf(z), z));
+}
