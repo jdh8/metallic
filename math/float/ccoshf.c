177eee6c6f2278a44d9eda2858fb9a750a3bf605
Chen-Pang He <chen.pang.he@jdh8.org>
2017-12-20 15:05:46 +0800

    Improve performance of ccosf

--- math/float/ccoshf.c
+++ math/float/ccoshf.c
@@ -6,9 +6,17 @@
  * Public License v. 2.0. If a copy of the MPL was not distributed
  * with this file, You can obtain one at http://mozilla.org/MPL/2.0/
  */
-#include "ccoshf.h"
+#include "cisf.h"
+#include "prec/expf.h"
+#include <complex.h>
 
 float _Complex ccoshf(float _Complex z)
 {
-    return __ccoshf(z);
+    double _Complex circular = __cisf(cimagf(z));
+
+    double exp = __prec_expf(z);
+    double cosh = 0.5 * exp + 0.5 / exp;
+    double sinh = 0.5 * exp - 0.5 / exp;
+
+    return CMPLXF(cosh * (double)circular, sinh * cimag(circular));
 }
