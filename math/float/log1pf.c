a1fbf057cb8174684e72a125a7b4dc5b7d2a38b1
Chen-Pang He <chen.pang.he@jdh8.org>
2017-12-21 23:27:30 +0800

    Correct log1pf

--- math/float/log1pf.c
+++ math/float/log1pf.c
@@ -7,24 +7,10 @@
  * with this file, You can obtain one at http://mozilla.org/MPL/2.0/
  */
 #include "quietf.h"
-#include "kernel/atanhf.h"
+#include "prec/log1pf.h"
 #include "../reinterpret.h"
-#include <float.h>
 #include <math.h>
-
-static double _finite(double x)
-{
-    const double ln2 = 0.69314718055994530942;
-
-    int64_t i = __bits(x);
-    int64_t exponent = (i - 0x3FE6A09E667F3BCD) >> (DBL_MANT_DIG - 1);
-    double y = __reinterpret(i - (exponent << (DBL_MANT_DIG - 1)));
-
-    if (exponent)
-        return 2 * __kernel_atanhf((y - 1) / (y + 1)) + exponent * ln2;
-    else
-        return 2 * __kernel_atanhf(x / (2 + x));
-}
+#include <stdint.h>
 
 float log1pf(float x)
 {
@@ -37,7 +23,7 @@ float log1pf(float x)
         return __quietf(x);
 
     if (i < 0x7F800000)
-        return _finite(x);
+        return __prec_log1pf(x);
 
     return x;
 }
---
+++ math/float/prec/log1pf.h
@@ -0,0 +1,25 @@
+/* This file is part of Metallic, a runtime library for WebAssembly.
+ *
+ * Copyright (C) 2017 Chen-Pang He <chen.pang.he@jdh8.org>
+ *
+ * This Source Code Form is subject to the terms of the Mozilla
+ * Public License v. 2.0. If a copy of the MPL was not distributed
+ * with this file, You can obtain one at http://mozilla.org/MPL/2.0/
+ */
+#include "../kernel/atanhf.h"
+#include "../../reinterpret.h"
+#include <float.h>
+
+inline double __prec_log1pf(double x)
+{
+    const double ln2 = 0.69314718055994530942;
+
+    int64_t i = __bits(1 + x);
+    int64_t exponent = (i - 0x3FE6A09E667F3BCD) >> (DBL_MANT_DIG - 1);
+    double y = __reinterpret(i - (exponent << (DBL_MANT_DIG - 1)));
+
+    if (exponent)
+        return 2 * __kernel_atanhf((y - 1) / (y + 1)) + exponent * ln2;
+    else
+        return 2 * __kernel_atanhf(x / (2 + x));
+}
