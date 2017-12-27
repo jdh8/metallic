ba18d8870b2052ce9a922a1e4cc63ccc6eda5a75
Chen-Pang He <chen.pang.he@jdh8.org>
2017-12-28 00:14:13 +0800

    Implement ceilf

---
+++ math/float/ceilf.c
@@ -0,0 +1,32 @@
+/* This file is part of Metallic, a runtime library for WebAssembly.
+ *
+ * Copyright (C) 2017 Chen-Pang He <chen.pang.he@jdh8.org>
+ *
+ * This Source Code Form is subject to the terms of the Mozilla
+ * Public License v. 2.0. If a copy of the MPL was not distributed
+ * with this file, You can obtain one at http://mozilla.org/MPL/2.0/
+ */
+#include "../reinterpret.h"
+#include "../round.h"
+#include <math.h>
+#include <float.h>
+
+float ceilf(float x)
+{
+#ifdef FAST_ROUNDING
+    return __builtin_ceilf(x);
+#else
+    int32_t bits = __bitsf(x);
+    int32_t magnitude = bits & 0x7FFFFFFF;
+
+    if (magnitude == 0 || magnitude >= 0x4B000000) /* 2**23 */
+        return x;
+
+    if (magnitude < 0x3F800000) /* 1 */
+        return bits >> 31;
+
+    int32_t mask = 0x007FFFFF >> ((magnitude >> 23) - 127);
+
+    return __reinterpretf((bits + (bits >= 0) * mask) & ~mask);
+#endif
+}
