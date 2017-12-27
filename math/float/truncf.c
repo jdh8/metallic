1c7eaca9c8f3ce93e6b287e7295ef8042250c708
Chen-Pang He <chen.pang.he@jdh8.org>
2017-12-27 20:37:10 +0800

    Implement truncf

---
+++ math/float/truncf.c
@@ -0,0 +1,31 @@
+/* This file is part of Metallic, a runtime library for WebAssembly.
+ *
+ * Copyright (C) 2017 Chen-Pang He <chen.pang.he@jdh8.org>
+ *
+ * This Source Code Form is subject to the terms of the Mozilla
+ * Public License v. 2.0. If a copy of the MPL was not distributed
+ * with this file, You can obtain one at http://mozilla.org/MPL/2.0/
+ */
+#include "../reinterpret.h"
+#include "../round.h"
+#include <math.h>
+#include <float.h>
+
+float truncf(float x)
+{
+#ifdef FAST_ROUNDING
+    return __builtin_truncf(x);
+#else
+    const int32_t mask = 0xFF800000;
+    int32_t bits = __bitsf(x);
+    int32_t magnitude = bits & 0x7FFFFFFF;
+
+    if (magnitude < 0x3F800000) /* 1 */
+        return __reinterpretf(bits & 0x80000000);
+
+    if (magnitude < 0x4B000000) /* 2**23 */
+        return __reinterpretf(mask >> ((magnitude >> 23) - 127) & bits);
+
+    return x;
+#endif
+}
