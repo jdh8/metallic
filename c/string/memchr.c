71515a4cd89f282b3f2d1d7e5bab432b3377566d
Chen-Pang He <chen.pang.he@jdh8.org>
2017-12-30 05:16:28 +0800

    Improve semantics in memchr

--- c/string/memchr.c
+++ c/string/memchr.c
@@ -13,27 +13,27 @@ void* memchr(const void* source, int c, size_t length)
 {
     const unsigned char* src = source;
 
-    for (; length && (uintptr_t) src % sizeof(uint64_t); --length)
-        if (*src++ == c)
-            return (void*)--src;
+    for (; length-- && (uintptr_t)src % sizeof(uint64_t); ++src)
+        if (*src == c)
+            return (unsigned char*)src;
 
-    const uint64_t* vsrc = (const uint64_t*) src;
-    const uint64_t magic = 0x7EFEFEFEFEFEFEFFu;
-    const uint64_t mask = 0x0101010101010101u * (unsigned char) c;
+    const uint64_t* vector = (const uint64_t*)src;
+    const uint64_t magic = 0x7EFEFEFEFEFEFEFF;
+    const uint64_t mask = 0x0101010101010101 * (unsigned char)c;
 
     for (; length >= sizeof(uint64_t); length -= sizeof(uint64_t)) {
-        src = (const unsigned char*) vsrc;
-        uint64_t word = *vsrc++ ^ mask;
+        src = (const unsigned char*)vector;
+        uint64_t word = *vector++ ^ mask;
 
         if (((word + magic) ^ ~word) & ~magic)
             for (int k = 0; k < sizeof(uint64_t); ++k)
                 if (src[k] == c)
-                    return (void*)(src + k);
+                    return (unsigned char*)(src + k);
     }
 
-    for (; length; --length)
-        if (*src++ == c)
-            return (void*)--src;
+    for (; length--; ++src)
+        if (*src == c)
+            return (unsigned char*)src;
 
     return NULL;
 }
