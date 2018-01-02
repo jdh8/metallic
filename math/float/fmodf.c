f9f62e08987eeb424ec25c413dc71f6dc347a925
Chen-Pang He <chen.pang.he@jdh8.org>
2018-01-02 18:42:06 +0800

    Implement precise fmodf

--- math/float/fmodf.c
+++ math/float/fmodf.c
@@ -6,13 +6,61 @@
  * Public License v. 2.0. If a copy of the MPL was not distributed
  * with this file, You can obtain one at http://mozilla.org/MPL/2.0/
  */
-#include <math.h>
+#include "normalizef.h"
+#include "../reinterpret.h"
+
+static int32_t _mantissa(int32_t a)
+{
+    return (a & 0x007FFFFF) | 0x00800000;
+}
+
+static int32_t _load(int32_t mantissa, int32_t template)
+{
+    if (mantissa == 0)
+        return mantissa;
+
+    int32_t shift = __builtin_clz(mantissa) - 8;
+    int32_t bias = -shift << 23 | (mantissa << shift & 0x007FFFFF);
+    int32_t normalized = bias + (template & 0x7F800000);
+
+    if (normalized < 0x00800000)
+        return _mantissa(normalized) >> -((normalized >> 23) + 1);
+
+    return normalized;
+}
+
+static int32_t _finite(int32_t a, int32_t b)
+{
+    unsigned exp = (a >> 23) - (b >> 23);
+    uint_least32_t divisor = _mantissa(b);
+    uint_least64_t remainder = ((uint_least64_t)_mantissa(a) << (exp & 31)) % divisor;
+    uint_least64_t coeff = 0x100000000 % divisor;
+
+    for (exp >>= 5; exp; exp >>= 1) {
+        if (exp & 1)
+            remainder = remainder * coeff % divisor;
+        coeff = coeff * coeff % divisor;
+    }
+
+    return _load(remainder, b);
+}
 
 float fmodf(float numerator, float denominator)
 {
-    double a = numerator;
-    double b = denominator;
-    double q = trunc(a / b);
+    int32_t a = __bitsf(numerator);
+    int32_t b = __bitsf(denominator) & 0x7FFFFFFF;
+    int32_t s = a & 0x80000000;
 
-    return a - b * q;
+    a &= 0x7FFFFFFF;
+
+    if (a >= 0x7F800000)
+        return __reinterpretf(a | 0x00400000);
+
+    if (b > 0x7F800000)
+        return __reinterpretf(b | s);
+
+    if (a < b || b == 0)
+        return numerator;
+
+    return __reinterpretf(_finite(__normalizef(a), __normalizef(b)) | s);
 }
