0a564370c582321a7985c9bcd7a54610b42bc79a
Chen-Pang He <chen.pang.he@jdh8.org>
2018-01-12 15:59:28 +0800

    Implement strncat

---
+++ c/string/strncat.c
@@ -0,0 +1,18 @@
+/* This file is part of Metallic, a runtime library for WebAssembly.
+ *
+ * Copyright (C) 2017 Chen-Pang He <chen.pang.he@jdh8.org>
+ *
+ * This Source Code Form is subject to the terms of the Mozilla
+ * Public License v. 2.0. If a copy of the MPL was not distributed
+ * with this file, You can obtain one at http://mozilla.org/MPL/2.0/
+ */
+#include <string.h>
+
+char* strncat(char destination[static restrict 1], const char source[static restrict 1], size_t length)
+{
+    char* output = destination + strlen(destination);
+
+    while (length-- && (*output++ = *source++));
+
+    return destination;
+}
