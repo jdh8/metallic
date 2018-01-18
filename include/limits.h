286fd648d71175ce94811bd82ec12fdb0116f843
Chen-Pang He <chen.pang.he@jdh8.org>
2018-01-18 14:58:53 +0800

    Make the preprocessor happy with our limits.h by assuming signed integers have the same bits as their unsigned counterparts.

--- include/limits.h
+++ include/limits.h
@@ -14,20 +14,11 @@
 /* Unicode may consume 32 bits for a character. */
 #define MB_LEN_MAX (32 / CHAR_BIT + !!(32 % CHAR_BIT))
 
-/* Minima and maxima can be found in a cross-platform way due to the following
- * facts:
- *
- * 1. Unsigned integers wrap around, so (Unsigned)-1 is the maximum.
- * 2. Signed integer representation must be 2's complement, 1's complement,
- *    or sign-magnitude.
- * 2.a. Minimum is (-maximum - 1) for 2's complement or -maximum otherwise.
- * 2.b. -1 == ~0 is true iff the representation is 2's complement.
- */
-#define SCHAR_MIN (-SCHAR_MAX - (-(signed char)1 == ~(signed char)0))
+#define SCHAR_MIN (-SCHAR_MAX - (-1 == ~0))
 #define SCHAR_MAX __SCHAR_MAX__
-#define UCHAR_MAX ((unsigned char)-1)
+#define UCHAR_MAX (SCHAR_MAX * 2u + 1)
 
-#if (char)-1 < 0  /* Signed */
+#if -'\1' < 0
 # define CHAR_MIN SCHAR_MIN
 # define CHAR_MAX SCHAR_MAX
 #else
@@ -35,20 +26,20 @@
 # define CHAR_MAX UCHAR_MAX
 #endif
 
-#define SHRT_MIN  (-SHRT_MAX - (-(short)1 == ~(short)0))
+#define SHRT_MIN  (-SHRT_MAX - (-1 == ~0))
 #define SHRT_MAX  __SHRT_MAX__
-#define USHRT_MAX ((unsigned short)-1)
+#define USHRT_MAX (SHRT_MAX * 2u + 1)
 
 #define INT_MIN  (-INT_MAX - (-1 == ~0))
 #define INT_MAX  __INT_MAX__
-#define UINT_MAX (-1u)
+#define UINT_MAX (INT_MAX * 2u + 1)
 
 #define LONG_MIN  (-LONG_MAX - (-1L == ~0L))
 #define LONG_MAX  __LONG_MAX__
-#define ULONG_MAX (-1UL)
+#define ULONG_MAX (LONG_MAX * 2u + 1)
 
 #define LLONG_MIN  (-LLONG_MAX - (-1LL == ~0LL))
 #define LLONG_MAX  __LONG_LONG_MAX__
-#define ULLONG_MAX (-1ULL)
+#define ULLONG_MAX (LLONG_MAX * 2u + 1)
 
 #endif /* limits.h */
