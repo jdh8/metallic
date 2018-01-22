31bf2109d4f25c8864c1ffa5b1a4496d8d2c8b46
Chen-Pang He <chen.pang.he@jdh8.org>
2018-01-23 00:11:50 +0800

    Remove repeated comments

--- math/float/normalizef.h
+++ math/float/normalizef.h
@@ -14,21 +14,10 @@
 /*!
  * \brief Normalize nonzero magnitude
  *
- * Notation of subnormal numbers is a space-saving technique in IEEE 754.
- * However, processing subnormal numbers requires an extra step to get correct
- * exponents and significands.  This function provides that step.
- *
- * The signbits of results safely join exponent bits because negative
- * representations were unused.  Infinite and normal numbers remain intact
- * since exponent bias is preserved.  The exponent representation of a
- * subnormal number becomes 0 or negative to preserve exponent bias.
- *
- * Zero is excluded from the domain of this function because one half of the
- * smallest normal number is already mapped to 0 and there is no other suitable
- * result for 0.
- *
  * \param i - Representation of a nonzero magnitude
  * \return  Normalized magnitude
+ *
+ * \sa __normalize() for details
  */
 inline int32_t __normalizef(int32_t i)
 {
