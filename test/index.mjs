0b1beab9e11fb6b9d802afa61c364fd04dfb7667
Chen-Pang He <chen.pang.he@jdh8.org>
2018-04-23 02:42:19 +0800

    Call _start by hand since llvm has not supported WebAssembly start function yet

--- test/index.mjs
+++ test/index.mjs
@@ -1,3 +1,3 @@
 import fs from "fs";
 
-process.argv.slice(2).map(async path => await WebAssembly.instantiate(fs.readFileSync(path)));
+process.argv.slice(2).map(async path => (await WebAssembly.instantiate(fs.readFileSync(path))).instance.exports._start());
