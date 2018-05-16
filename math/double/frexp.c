af9ef79b271b4df73b6cce17192c3904cd515ebe
Chen-Pang He <chen.pang.he@jdh8.org>
2018-05-16 15:31:11 +0800

    Impplement frexp

--- math/double/frexp.c
+++ math/double/frexp.c
@@ -1,28 +1,28 @@
 /* This file is part of Metallic, a runtime library for WebAssembly.
  *
- * Copyright (C) 2017 Chen-Pang He <chen.pang.he@jdh8.org>
+ * Copyright (C) 2018 Chen-Pang He <chen.pang.he@jdh8.org>
  *
  * This Source Code Form is subject to the terms of the Mozilla
  * Public License v. 2.0. If a copy of the MPL was not distributed
  * with this file, You can obtain one at http://mozilla.org/MPL/2.0/
  */
-#include "normalizef.h"
+#include "normalize.h"
 #include "../reinterpret.h"
 #include <float.h>
 #include <math.h>
 #include <stdint.h>
 
-float frexpf(float x, int exp[static 1])
+double frexp(double x, int exp[static 1])
 {
-    int32_t i = reinterpret(int32_t, fabsf(x));
+    int64_t i = reinterpret(int64_t, fabs(x));
 
-    if (i == 0 || i >= 0x7F800000) {
+    if (i == 0 || i >= 0x7FF0000000000000) {
         *exp = 0;
         return x;
     }
 
-    i = __normalizef(i);
-    *exp = (i >> (FLT_MANT_DIG - 1)) - 127;
+    i = __normalize(i);
+    *exp = (i >> (DBL_MANT_DIG - 1)) - 1023;
 
-    return copysignf(reinterpret(float, (i & 0x007FFFFF) | 0x3F000000), x);
+    return copysign(reinterpret(double, (i & 0x000FFFFFFFFFFFFF) | 0x3FE0000000000000), x);
 }
