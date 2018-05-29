cf7fd16df1c32d4a95bb08cb0528e8c34f7d69ad
Chen-Pang He <chen.pang.he@jdh8.org>
2018-05-29 19:29:45 +0800

    It took too much time to test expf on an old box

--- test/native/float/expf.c
+++ test/native/float/expf.c
@@ -5,13 +5,13 @@ int main(void)
 {
     const int32_t threshold = 0x42AEAC50; // 126 log(2)
 
-    for (int32_t i = 0; i < threshold; i += 69) {
+    for (int32_t i = 0; i < threshold; i += 543) {
         float x = reinterpret(float, i);
         verify(approx(expf(x), exp(x)), x);
         verify(approx(expf(-x), exp(-x)), x);
     }
 
-    for (int32_t i = threshold; i < 0x7F800000; i += 77) {
+    for (int32_t i = threshold; i < 0x7F800000; i += 777) {
         float x = reinterpret(float, i);
         verify(approxf(expf(x), exp(x)), x);
         verify(approxf(expf(-x), exp(-x)), x);
