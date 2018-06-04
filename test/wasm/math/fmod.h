7f85df8fbd5b58dac44e6ba184d07b5d95303c1c
Chen-Pang He <chen.pang.he@jdh8.org>
2018-06-04 23:30:31 +0800

    Test fmodf for normal values

--- test/wasm/math/fmod.h
+++ test/wasm/math/fmod.h
@@ -8,6 +8,27 @@
  */
 #define fmod SUFFIX(fmod)
 
+static void test_fmod_finite(Unsigned a, Unsigned b)
+{
+    Scalar x = reinterpret(Scalar, a);
+    Scalar y = reinterpret(Scalar, b);
+
+    for (Unsigned r = a % b; x < INFINITY; r = 2 * r % b) {
+        metallic_assert(reinterpret(Unsigned, fmod(x, y)) == r);
+        metallic_assert(reinterpret(Unsigned, fmod(x, -y)) == r);
+        x *= 2;
+    }
+
+    x = a;
+    y = b;
+
+    for (Unsigned r = a % b; x < INFINITY; r = 2 * r % b) {
+        metallic_assert(fmod(x, y) == r);
+        metallic_assert(fmod(-x, y) == -(Scalar)r);
+        x *= 2;
+    }
+}
+
 __attribute__((constructor))
 static void test_fmod(void)
 {
@@ -29,6 +50,8 @@ static void test_fmod(void)
 
     for (Unsigned i = 0; i < max; i += step) {
         Scalar x = reinterpret(Scalar, i);
+        metallic_assert(reinterpret(Unsigned, fmod(x, LIMITS(TRUE_MIN))) == 0);
+        metallic_assert(reinterpret(Unsigned, fmod(-x, LIMITS(TRUE_MIN))) == reinterpret(Unsigned, -(Scalar)0));
         metallic_assert(reinterpret(Unsigned, fmod(x, INFINITY)) == reinterpret(Unsigned, x));
         metallic_assert(reinterpret(Unsigned, fmod(x, -INFINITY)) == reinterpret(Unsigned, x));
         metallic_assert(isnan(fmod(x, NAN)));
@@ -36,6 +59,11 @@ static void test_fmod(void)
         metallic_assert(isnan(fmod(-INFINITY, x)));
         metallic_assert(isnan(fmod(NAN, x)));
     }
+
+    test_fmod_finite(1, 7);
+    test_fmod_finite(31, 64);
+    test_fmod_finite(101, 77);
+    test_fmod_finite(101325, 96485);
 }
 
 /* vim: set ft=c: */
