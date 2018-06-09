2afa08bd0cfc72019a806fa4458448af043467ce
Chen-Pang He <chen.pang.he@jdh8.org>
2018-06-10 04:32:11 +0800

    Fix remquof

--- src/math/float/remquof.c
+++ src/math/float/remquof.c
@@ -1,24 +1,22 @@
 /* This file is part of Metallic, a runtime library for WebAssembly.
  *
- * Copyright (C) 2017 Chen-Pang He <chen.pang.he@jdh8.org>
+ * Copyright (C) 2017, 2018 Chen-Pang He <chen.pang.he@jdh8.org>
  *
  * This Source Code Form is subject to the terms of the Mozilla
  * Public License v. 2.0. If a copy of the MPL was not distributed
  * with this file, You can obtain one at http://mozilla.org/MPL/2.0/
  */
 #include "../reinterpret.h"
+#include "../round.h"
 #include <math.h>
-#include <stdint.h>
 
 float remquof(float numerator, float denominator, int quotient[static 1])
 {
-    if ((reinterpret(uint32_t, denominator) & 0x7FFFFFFF) < 0x7E000000)
-        numerator = fmodf(numerator, 8 * denominator);
+    float r = fmodf(numerator, 8 * denominator);
+    float q = __rintf(r / denominator);
 
-    float q = rintf(numerator / denominator);
-    float r = numerator - denominator * (double)q;
-
-    *quotient = q;
+    if ((*quotient = q))
+        r -= denominator * (double)q;
 
     return r ? r : copysignf(r, numerator);
 }
