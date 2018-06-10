fbe47df96f32e68ce9f33f7c69bb80da8cd7126c
Chen-Pang He <chen.pang.he@jdh8.org>
2018-06-11 04:10:34 +0800

    Purify remquof by removing double arithmetics

--- src/math/float/remquof.c
+++ src/math/float/remquof.c
@@ -7,16 +7,52 @@
  * with this file, You can obtain one at http://mozilla.org/MPL/2.0/
  */
 #include "../reinterpret.h"
-#include "../round.h"
 #include <math.h>
+#include <float.h>
+#include <stdint.h>
 
 float remquof(float numerator, float denominator, int quotient[static 1])
 {
-    float r = fmodf(numerator, 8 * denominator);
-    float q = __rintf(r / denominator);
+    int32_t from = reinterpret(int32_t, numerator);
+    int q = 0;
 
-    if ((*quotient = q))
-        r -= denominator * (double)q;
+    numerator = fmodf(fabsf(numerator), 8 * denominator);
+    denominator = fabsf(denominator);
 
-    return r ? r : copysignf(r, numerator);
+    if (numerator >= 4 * denominator) {
+        numerator -= 4 * denominator;
+        q |= 4;
+    }
+
+    if (numerator >= 2 * denominator) {
+        numerator -= 2 * denominator;
+        q |= 2;
+    }
+
+    if (denominator < 2 * FLT_MIN) {
+        if (2 * numerator > denominator) {
+            numerator -= denominator;
+            ++q;
+
+            if (2 * numerator >= denominator) {
+                numerator -= denominator;
+                ++q;
+            }
+        }
+    }
+    else if (numerator > 0.5f * denominator) {
+        numerator -= denominator;
+        ++q;
+
+        if (numerator >= 0.5f * denominator) {
+            numerator -= denominator;
+            ++q;
+        }
+    }
+
+    *quotient = from < 0 ? -q : q;
+
+    uint32_t to = (from & 0x80000000u) ^ reinterpret(uint32_t, numerator);
+
+    return reinterpret(float, to);
 }
