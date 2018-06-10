9fb717ab302452f47877176ef8836b2d2ab8ef37
Chen-Pang He <chen.pang.he@jdh8.org>
2018-06-10 23:05:37 +0800

    Improve remainderf

--- src/math/float/remainderf.c
+++ src/math/float/remainderf.c
@@ -7,16 +7,35 @@
  * with this file, You can obtain one at http://mozilla.org/MPL/2.0/
  */
 #include "../reinterpret.h"
-#include "../round.h"
 #include <math.h>
+#include <float.h>
+#include <stdint.h>
+
+static float _quadrant(float numerator, float denominator)
+{
+    numerator = fmodf(numerator, 2 * denominator);
+
+    if (denominator < 2 * FLT_MIN) {
+        if (2 * numerator > denominator) {
+            numerator -= denominator;
+            if (2 * numerator >= denominator)
+                numerator -= denominator;
+        }
+    }
+    else if (numerator > 0.5f * denominator) {
+        numerator -= denominator;
+        if (numerator >= 0.5f * denominator)
+            numerator -= denominator;
+    }
+
+    return numerator;
+}
 
 float remainderf(float numerator, float denominator)
 {
-    float r = fmodf(numerator, 2 * denominator);
-    float q = __rintf(r / denominator);
+    float r = _quadrant(fabsf(numerator), fabsf(denominator));
+    uint32_t i = reinterpret(uint32_t, r);
+    uint32_t sign = reinterpret(uint32_t, numerator) & 0x80000000;
 
-    if (q)
-        r -= denominator * (double)q;
-
-    return r ? r : copysignf(r, numerator);
+    return reinterpret(float, i ^ sign);
 }
