fedf494e70e446d4d5ea8b6702f204b1297df1c2
Chen-Pang He <chen.pang.he@jdh8.org>
2018-06-27 17:52:17 +0800

    Improve error handling on wasm tests

--- test/wasm/index.mjs
+++ test/wasm/index.mjs
@@ -1,3 +1,12 @@
 import fs from "fs";
 
-WebAssembly.instantiate(fs.readFileSync(process.argv[2])).then(module => module.instance.exports._start());
+WebAssembly.instantiate(fs.readFileSync(process.argv[2])).then(module =>
+{
+	try {
+		module.instance.exports._start();
+	}
+	catch (error) {
+		console.error(error);
+		process.exit(1);
+	}
+});
