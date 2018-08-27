56150f56c5105df816a59c1236b5bf4dcdae6385
Chen-Pang He <chen.pang.he@jdh8.org>
2018-08-28 01:32:49 +0800

    Fix atanf

--- src/math/float/atanf.c
+++ src/math/float/atanf.c
@@ -1,6 +1,6 @@
 /* This file is part of Metallic, a runtime library for WebAssembly.
  *
- * Copyright (C) 2017 Chen-Pang He <chen.pang.he@jdh8.org>
+ * Copyright (C) 2017, 2018 Chen-Pang He <chen.pang.he@jdh8.org>
  *
  * This Source Code Form is subject to the terms of the Mozilla
  * Public License v. 2.0. If a copy of the MPL was not distributed
@@ -9,9 +9,13 @@
 #include "kernel/atanf.h"
 #include <math.h>
 
-float atanf(float x)
+static float _atanf(float x)
 {
     const double pi_2 = 1.57079632679489661923;
 
-    return fabsf(x) > 1 ? pi_2 - __kernel_atanf(1.0 / x) : __kernel_atanf(x);
+    return fabsf(x) > 1 ? copysign(pi_2, x) - __kernel_atanf(1.0 / x) : __kernel_atanf(x);
 }
+
+#ifdef _METALLIC
+float atanf(float x) { return _atanf(x); }
+#endif
---
+++ test/native/float/atanf.c
@@ -0,0 +1,28 @@
+/* This file is part of Metallic, a runtime library for WebAssembly.
+ *
+ * Copyright (C) 2018 Chen-Pang He <chen.pang.he@jdh8.org>
+ *
+ * This Source Code Form is subject to the terms of the Mozilla
+ * Public License v. 2.0. If a copy of the MPL was not distributed
+ * with this file, You can obtain one at http://mozilla.org/MPL/2.0/
+ */
+#include "common.h"
+#include "../../../src/math/float/atanf.c"
+#include <assert.h>
+
+int main(void)
+{
+    assert(_atanf(INFINITY) == (float)atan(INFINITY));
+    assert(_atanf(-INFINITY) == (float)atan(-INFINITY));
+
+    for (int32_t i = 0; i < 0x7F800000; i += 81) {
+        float x = reinterpret(float, i);
+        verify(approx(_atanf(x), atan(x)), x);
+    }
+
+    for (uint32_t i = 0x7FC00000; i < 0x80000000u; i += 81) {
+        float x = reinterpret(float, i);
+        assert(isnan(_atanf(x)));
+        assert(isnan(_atanf(-x)));
+    }
+}
