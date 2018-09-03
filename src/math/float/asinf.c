f125fb26fc4e2126cc2e5303a56c1a257c8ad9fa
Chen-Pang He <chen.pang.he@jdh8.org>
2018-09-03 16:54:21 +0800

    Optimize asinf

--- src/math/float/asinf.c
+++ src/math/float/asinf.c
@@ -1,6 +1,6 @@
 /* This file is part of Metallic, a runtime library for WebAssembly.
  *
- * Copyright (C) 2017 Chen-Pang He <chen.pang.he@jdh8.org>
+ * Copyright (C) 2017, 2018 Chen-Pang He <chen.pang.he@jdh8.org>
  *
  * This Source Code Form is subject to the terms of the Mozilla
  * Public License v. 2.0. If a copy of the MPL was not distributed
@@ -12,7 +12,7 @@
  *
  * This function computes arccosine in a domain of \f$ [0.5, 1] \f$.
  * The result is guaranteed to be faithfully rounded in \c float,
- * whose relative error is controlled within 3.669929e-9.
+ * whose relative error is controlled within 6.394937e-9.
  *
  * If \f$ x > 1 \f$, this function returns NaN to indicate complex result.
  * If \f$ x < 0.5 \f$, the result is inaccurate.
@@ -20,82 +20,58 @@
  * \param x - The argument in \f$ [0.5, \infty] \f$
  * \return  Approximate \f$ \arccos x \f$ as precise as \c float.
  */
-static double _acos22(double x)
+static double _acos(double x)
 {
-    const double n[] = {
-        19.3409975360328623,
-        11.6478870037997229,
-         0.700950333090353189
-    };
-
-    const double d[] = {
-        12.3131674101366886,
-         9.09492966503933472,
-         1
+    const double c[] = {
+        1.57060812392777102,
+       -2.12862161326166423e-1,
+        8.21583834379219260e-2,
+       -3.53431994400216517e-2,
+        1.15390721259672731e-2,
+       -1.88666274731420876e-3
     };
 
     double xx = x * x;
-    double num = n[0] + n[1] * x + n[2] * xx;
-    double den = d[0] + d[1] * x + d[2] * xx;
 
-    return sqrt(1 - x) * num / den;
+    return sqrt(1 - x) * (c[0] + c[1] * x + (c[2] + c[3] * x) * xx + (c[4] + c[5] * x) * (xx * xx));
 }
 
 /*!
  * \brief Kernel of asinf
  *
- * This function computes arcsine in a domain of \f$ [0, 0.5] \f$.
+ * This function computes arcsine in a domain of \f$ [-0.5, 0.5] \f$.
  * The result is guaranteed to be faithfully rounded in \c float,
- * whose relative error is controlled within 2.595032e-9.
+ * whose relative error is controlled within 4.521903e-9.
  *
- * If \a x is outside of \f$ [0, 0.5] \f$, the result is inaccurate.
+ * If \a x is outside of \f$ [-0.5, 0.5] \f$, the result is inaccurate.
  *
- * \param x - The argument in \f$ [0, 0.5] \f$
+ * \param x - The argument in \f$ [-0.5, 0.5] \f$
  * \return  Approximate \f$ \arcsin x \f$.
  */
-static double _asin22(double x)
+static float _asin(double x)
 {
-    double n[] = {
-        80.3271869581482272,
-        72.2722811622844352,
-        11.0979939649070095
-    };
-
-    double d[] = {
-        5.60202426879400611,
-       -5.54746483251966753,
-        1
+    double c[] = {
+        9.99999995478096602e-1,
+        1.66667922213007301e-1,
+        7.49436750021161056e-2,
+        4.55576929740685241e-2,
+        2.38239365869205885e-2,
+        4.26903047180133904e-2
     };
 
     double x2 = x * x;
     double x4 = x2 * x2;
-    double num = n[0] + n[1] * x2 + n[2] * x4;
-    double den = d[0] + d[1] * x2 + d[2] * x4;
 
-    return x * num / den;
-}
-
-/*!
- * \brief Kernel of asinf
- *
- * This function computes arcsine in a domain of \f$ [0, 1] \f$.
- *
- * If \f$ x > 1 \f$, this function returns NaN to indicate complex result.
- * If \f$ x < 0 \f$, the result is inaccurate.
- *
- * \param x - The argument in \f$ [0, \infty] \f$
- * \return  Approximate \f$ \arcsin x \f$.
- */
-static float _kernel(float x)
-{
-    const double pi_2 = 1.57079632679489661923;
-
-    return x < 0.5f ? _asin22(x) : pi_2 - _acos22(x);
+    return x * (c[0] + c[1] * x2 + (c[2] + c[3] * x2) * x4 + (c[4] + c[5] * x2) * (x4 * x4));
 }
 
 static float _asinf(float x)
 {
-    return copysignf(_kernel(fabsf(x)), x);
+    const double pi_2 = 1.57079632679489661923;
+
+    float r = fabsf(x);
+
+    return r < 0.5f ? _asin(x) : copysignf(pi_2 - _acos(r), x);
 }
 
 #ifdef _METALLIC
