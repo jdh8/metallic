5e70002e5f0f40d51c7e0572db40e35ceb6dadce
Chen-Pang He <chen.pang.he@jdh8.org>
2018-09-06 11:58:41 +0800

    Test erff

---
+++ test/native/float/erff.c
@@ -0,0 +1,32 @@
+/* This file is part of Metallic, a runtime library for WebAssembly.
+ *
+ * Copyright (C) 2018 Chen-Pang He <chen.pang.he@jdh8.org>
+ *
+ * This Source Code Form is subject to the terms of the Mozilla
+ * Public License v. 2.0. If a copy of the MPL was not distributed
+ * with this file, You can obtain one at http://mozilla.org/MPL/2.0/
+ */
+#include "common.h"
+#include "../../../src/math/float/erff.c"
+#include <assert.h>
+
+int main(void)
+{
+    assert(_erff(INFINITY) == 1);
+    assert(_erff(-INFINITY) == -1);
+
+    for (uint32_t i = 0; i < 0x7F800000; i += 77) {
+        float x = reinterpret(float, i);
+        float y = _erff(x);
+        verify(approx(y, erf(x)), x);
+        verify(identical(-y, _erff(-x)), x);
+    }
+
+    for (uint32_t i = 0x7FC00000; i < 0x80000000u; i += 81) {
+        float x = reinterpret(float, i);
+        assert(isnan(_erff(x)));
+        assert(isnan(_erff(-x)));
+    }
+}
+
+double __kernel_erfcf(double x);
