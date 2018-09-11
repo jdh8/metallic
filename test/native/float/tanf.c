a162fa268b018487d961d78ad24ff9acf649477c
Chen-Pang He <chen.pang.he@jdh8.org>
2018-09-11 12:40:06 +0800

    Test tanf

---
+++ test/native/float/tanf.c
@@ -0,0 +1,29 @@
+/* This file is part of Metallic, a runtime library for WebAssembly.
+ *
+ * Copyright (C) 2018 Chen-Pang He <chen.pang.he@jdh8.org>
+ *
+ * This Source Code Form is subject to the terms of the Mozilla
+ * Public License v. 2.0. If a copy of the MPL was not distributed
+ * with this file, You can obtain one at http://mozilla.org/MPL/2.0/
+ */
+#include "common.h"
+#include "../../../src/math/float/tanf.c"
+#include "../../../src/math/rem_pio2.c"
+#include <assert.h>
+
+int main(void)
+{
+    assert(isnan(tanf(INFINITY)));
+    assert(isnan(tanf(-INFINITY)));
+
+    for (int32_t i = 0; i < 0x7F800000; i += 81) {
+        float x = reinterpret(float, i);
+        verify(approx(tanf(x), tan(x)), x);
+    }
+
+    for (uint32_t i = 0x7FC00000; i < 0x80000000u; i += 81) {
+        float x = reinterpret(float, i);
+        assert(isnan(tanf(x)));
+        assert(isnan(tanf(-x)));
+    }
+}
