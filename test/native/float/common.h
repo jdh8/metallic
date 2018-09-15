4c8a566280ea16396f660ac75f30f20e515c4a18
Chen-Pang He <chen.pang.he@jdh8.org>
2018-09-15 17:47:17 +0800

    Test helper for complex functions

--- test/native/float/common.h
+++ test/native/float/common.h
@@ -2,7 +2,6 @@
 #include <stdint.h>
 #include <float.h>
 #include <inttypes.h>
-#include <stdlib.h>
 #include <stdio.h>
 
 static inline _Bool approx(double x, double y)
@@ -22,10 +21,19 @@ static inline _Bool approxf(float x, float y)
     return a - b + 1 <= 2;
 }
 
+double cimag(double _Complex);
+
+static inline _Bool capprox(double _Complex x, double _Complex y)
+{
+    return approx(x, y) && approx(cimag(x), cimag(y));
+}
+
 static inline _Bool identical(float x, float y)
 {
     return reinterpret(uint32_t, x) == reinterpret(uint32_t, y);
 }
+
+_Noreturn void abort(void);
 
 #define verify(cond, x) if (!(cond)) {                                                               \
     float y = x;                                                                                     \
