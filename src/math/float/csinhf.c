f16953a525d26735cc98e6fc66adcc79fc356c7a
Chen-Pang He <chen.pang.he@jdh8.org>
2018-09-21 03:38:11 +0800

    Improve accuracy of complex trig functions

--- src/math/float/csinhf.c
+++ src/math/float/csinhf.c
@@ -1,22 +1,22 @@
 /* This file is part of Metallic, a runtime library for WebAssembly.
  *
- * Copyright (C) 2017 Chen-Pang He <chen.pang.he@jdh8.org>
+ * Copyright (C) 2018 Chen-Pang He <chen.pang.he@jdh8.org>
  *
  * This Source Code Form is subject to the terms of the Mozilla
  * Public License v. 2.0. If a copy of the MPL was not distributed
  * with this file, You can obtain one at http://mozilla.org/MPL/2.0/
  */
 #include "cisf.h"
-#include "prec/expf.h"
+#include "prec/expm1f.h"
 #include <complex.h>
 
 float _Complex csinhf(float _Complex z)
 {
     double _Complex circular = _cisf(cimagf(z));
 
-    double exp = _expf(z);
-    double cosh = 0.5 * exp + 0.5 / exp;
-    double sinh = 0.5 * exp - 0.5 / exp;
+    double y = _expm1f(fabsf((float)z));
+    double cosh = 0.5 * (y + 1) + 0.5 / (y + 1);
+    double sinh = copysign(y * (0.5 + 0.5 / (y + 1)), z);
 
     return CMPLXF(sinh * (double)circular, cosh * cimag(circular));
 }
