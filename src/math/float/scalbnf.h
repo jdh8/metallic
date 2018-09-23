5ec7de718d920c477625138397aed82fb00ac997
Chen-Pang He <chen.pang.he@jdh8.org>
2018-09-24 02:20:24 +0800

    Fix subnormal scalbnf

--- src/math/float/scalbnf.h
+++ src/math/float/scalbnf.h
@@ -1,37 +1,27 @@
 /* This file is part of Metallic, a runtime library for WebAssembly.
  *
- * Copyright (C) 2017, 2018 Chen-Pang He <chen.pang.he@jdh8.org>
+ * Copyright (C) 2018 Chen-Pang He <chen.pang.he@jdh8.org>
  *
  * This Source Code Form is subject to the terms of the Mozilla
  * Public License v. 2.0. If a copy of the MPL was not distributed
  * with this file, You can obtain one at http://mozilla.org/MPL/2.0/
  */
-#include "normalizef.h"
 #include "../reinterpret.h"
 #include <float.h>
-#include <math.h>
 #include <stdint.h>
+
+static double _coeff(Integer exp)
+{
+    if (exp >= 1024)
+        return DBL_MAX;
+
+    if (exp < -1022)
+        return 0x1p-1023;
+
+    return reinterpret(double, (uint64_t)(1023 + exp) << 52);
+}
 
 float SCALBNF(float x, Integer exp)
 {
-    int32_t i = reinterpret(int32_t, fabsf(x));
-
-    if (i == 0 || i >= 0x7F800000)
-        return x;
-
-    i = _normalizef(i);
-
-    Integer biased = exp + (i >> (FLT_MANT_DIG - 1));
-
-    if (biased >= 0xFF || biased < -FLT_MANT_DIG)
-        return x * (exp < 0 ? 0 : HUGE_VALF);
-
-    i &= 0x007FFFFF;
-
-    if (biased > 0)
-        i |= biased << (FLT_MANT_DIG - 1);
-    else
-        i = (i | 0x00800000) >> (1 - biased);
-
-    return copysignf(reinterpret(float, i), x);
+    return x * _coeff(exp);
 }
