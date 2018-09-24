3a98840360d3a111cc5f4607e2562699bdc105e8
Chen-Pang He <chen.pang.he@jdh8.org>
2018-09-24 15:35:19 +0800

    Move fdimf test to wasm because it can

--- test/wasm/float/fdimf.c
+++ test/wasm/float/fdimf.c
@@ -6,16 +6,17 @@
  * Public License v. 2.0. If a copy of the MPL was not distributed
  * with this file, You can obtain one at http://mozilla.org/MPL/2.0/
  */
-#include "common.h"
-#include "../../../src/math/float/fdimf.c"
-#include <assert.h>
+#include "../../../src/math/reinterpret.h"
+#include "../../assert.h"
+#include <math.h>
+#include <stdint.h>
 
 static void run(float x, float y)
 {
-    assert(fdimf(x, y) == x - y);
-    assert(fdimf(-y, -x) == x - y);
-    assert(reinterpret(uint32_t, fdimf(y, x)) == 0);
-    assert(reinterpret(uint32_t, fdimf(-x, -y)) == 0);
+    metallic_assert(fdimf(x, y) == x - y);
+    metallic_assert(fdimf(-y, -x) == x - y);
+    metallic_assert(reinterpret(uint32_t, fdimf(y, x)) == 0);
+    metallic_assert(reinterpret(uint32_t, fdimf(-x, -y)) == 0);
 }
 
 int main(void)
