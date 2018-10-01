4f84c5cf90fcc933adbb35fd98cbf2974f377e45
Chen-Pang He <chen.pang.he@jdh8.org>
2018-10-02 01:02:09 +0800

    Modularize testing tools for unary functions

--- test/native/float/common.h
+++ test/native/float/common.h
@@ -1,53 +1,27 @@
-#include "../../../src/math/reinterpret.h"
-#include <stdint.h>
-#include <float.h>
-#include <inttypes.h>
-#include <stdio.h>
+/* This file is part of Metallic, a runtime library for WebAssembly.
+ *
+ * Copyright (C) 2018 Chen-Pang He <chen.pang.he@jdh8.org>
+ *
+ * This Source Code Form is subject to the terms of the Mozilla
+ * Public License v. 2.0. If a copy of the MPL was not distributed
+ * with this file, You can obtain one at http://mozilla.org/MPL/2.0/
+ */
+#include "unary.h"
+
+#define verify2(cond, x, y) if (!(cond)) {                                                             \
+    fprintf(stderr, "Assertion `"#cond"' failed at (%g, %g) [0x%08"PRIx32" 0x%08"PRIx32"]\n",          \
+        (float)(x), (float)(y), reinterpret(uint32_t, (float)(x)), reinterpret(uint32_t, (float)(y))); \
+    abort();                                                                                           \
+}
 
 float cimagf(float _Complex);
-
-static inline _Bool identical(float x, float y)
-{
-    return reinterpret(uint32_t, x) == reinterpret(uint32_t, y);
-}
 
 static inline _Bool cidentical(float _Complex x, float _Complex y)
 {
     return reinterpret(uint64_t, x) == reinterpret(uint64_t, y);
 }
 
-static inline _Bool faithful(double x, double y)
-{
-    const uint64_t mask = (1L << (DBL_MANT_DIG - FLT_MANT_DIG)) - 1;
-    uint64_t a = reinterpret(uint64_t, x);
-    uint64_t b = reinterpret(uint64_t, y);
-
-    return a - b + mask <= 2 * mask;
-}
-
-static inline _Bool approx(float x, float y, unsigned tolerance)
-{
-    uint32_t a = reinterpret(uint32_t, x);
-    uint32_t b = reinterpret(uint32_t, y);
-
-    return a - b + tolerance <= 2 * tolerance;
-}
-
 static inline _Bool capprox(float _Complex x, float _Complex y, unsigned re, unsigned im)
 {
     return approx(x, y, re) && approx(cimagf(x), cimagf(y), im);
-}
-
-_Noreturn void abort(void);
-
-#define verify(cond, x) if (!(cond)) {                                    \
-    fprintf(stderr, "Assertion `"#cond"' failed at %g [0x%08"PRIx32"]\n", \
-        (float)(x), reinterpret(uint32_t, (float)(x)));                   \
-    abort();                                                              \
-}
-
-#define verify2(cond, x, y) if (!(cond)) {                                                             \
-    fprintf(stderr, "Assertion `"#cond"' failed at (%g, %g) [0x%08"PRIx32" 0x%08"PRIx32"]\n",          \
-        (float)(x), (float)(y), reinterpret(uint32_t, (float)(x)), reinterpret(uint32_t, (float)(y))); \
-    abort();                                                                                           \
 }
