9c7a2dabcef11865724ced584df07d6d83633edf
Chen-Pang He <chen.pang.he@jdh8.org>
2018-10-11 15:27:59 +0800

    Test ccosf

---
+++ test/wasm/float/ccosf.c
@@ -0,0 +1,23 @@
+/* This file is part of Metallic, a runtime library for WebAssembly.
+ *
+ * Copyright (C) 2018 Chen-Pang He <chen.pang.he@jdh8.org>
+ *
+ * This Source Code Form is subject to the terms of the Mozilla
+ * Public License v. 2.0. If a copy of the MPL was not distributed
+ * with this file, You can obtain one at http://mozilla.org/MPL/2.0/
+ */
+#include "../../assert.h"
+#include "cidentical.h"
+#include <complex.h>
+
+static void run(float x, float y)
+{
+    metallic_assert(cidentical(ccoshf(CMPLXF(-y, x)), ccosf(CMPLXF(x, y))));
+}
+
+int main(void)
+{
+    for (uint32_t j = 0; j < 1 << 12; ++j)
+        for (uint32_t i = 0; i < 1 << 12; ++i)
+            run(reinterpret(float, i << 20), reinterpret(float, j << 20));
+}
