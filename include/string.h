2819027f4e9ad863a3d376753febf80dd31c5192
Chen-Pang He <chen.pang.he@jdh8.org>
2018-10-16 02:18:02 +0800

    Complete header string.h

--- include/string.h
+++ include/string.h
@@ -9,14 +9,19 @@
 #ifndef _STRING_H
 #define _STRING_H
 
+#include "bits/null.h"
 #include "bits/specifiers.h"
 
 typedef __SIZE_TYPE__ size_t;
 
+#ifdef __cplusplus
+extern "C" {
+#endif
+
 int memcmp(const void*, const void*, size_t);
 void* memchr(const void*, int, size_t);
 void* memset(void*, int, size_t);
-void* memcpy(void*__restrict, const void*__restrict, size_t);
+void* memcpy(void* __restrict, const void* __restrict, size_t);
 void* memmove(void*, const void*, size_t);
 
 size_t strlen(const char[_STATIC 1]);
@@ -38,6 +43,15 @@ size_t strcspn(const char[_STATIC 1], const char[_STATIC 1]);
 char* strpbrk(const char[_STATIC 1], const char[_STATIC 1]);
 
 char* strstr(const char[_STATIC 1], const char[_STATIC 1]);
-char* strtok(char*__restrict, const char[_STATIC _RESTRICT 1]);
+char* strtok(char* __restrict, const char[_STATIC _RESTRICT 1]);
+
+int strcoll(const char[_STATIC 1], const char[_STATIC 1]);
+size_t strxfrm(char* __restrict, const char[_STATIC _RESTRICT 1], size_t);
+
+char* strerror(int);
+
+#ifdef __cplusplus
+}
+#endif
 
 #endif /* string.h */
