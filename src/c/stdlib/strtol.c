cb1bd048b866f5cd8ac5fd87f9bc21553a4c2685
Chen-Pang He <chen.pang.he@jdh8.org>
2018-10-16 09:01:32 +0800

    Remove unused includes

--- src/c/stdlib/strtol.c
+++ src/c/stdlib/strtol.c
@@ -6,8 +6,6 @@
  * Public License v. 2.0. If a copy of the MPL was not distributed
  * with this file, You can obtain one at http://mozilla.org/MPL/2.0/
  */
-#include <limits.h>
-
 typedef char Character;
 typedef long Integer;
 typedef unsigned long Unsigned;
