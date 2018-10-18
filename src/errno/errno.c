0d23659675adbf22d39a71c67690e0b40d4fef62
Chen-Pang He <chen.pang.he@jdh8.org>
2018-10-18 22:13:41 +0800

    Make errno thread local because clang finally supports that

--- src/errno/errno.c
+++ src/errno/errno.c
@@ -1,13 +1,14 @@
 /* This file is part of Metallic, a runtime library for WebAssembly.
  *
- * Copyright (C) 2017 Chen-Pang He <chen.pang.he@jdh8.org>
+ * Copyright (C) 2018 Chen-Pang He <chen.pang.he@jdh8.org>
  *
  * This Source Code Form is subject to the terms of the Mozilla
  * Public License v. 2.0. If a copy of the MPL was not distributed
  * with this file, You can obtain one at http://mozilla.org/MPL/2.0/
  */
+_Thread_local int errno = 0;
+
 int* __errno_location(void)
 {
-    static int errno;
     return &errno;
 }
