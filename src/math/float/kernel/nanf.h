d873ae7e29b7164b94b83e50870007da4813945a
Chen-Pang He <chen.pang.he@jdh8.org>
2018-10-19 06:51:46 +0800

    Implement nanf

--- src/math/float/kernel/nanf.h
+++ src/math/float/kernel/nanf.h
@@ -6,9 +6,16 @@
  * Public License v. 2.0. If a copy of the MPL was not distributed
  * with this file, You can obtain one at http://mozilla.org/MPL/2.0/
  */
-#include "kernel/nanf.h"
+#include "../../reinterpret.h"
+#include <ctype.h>
+#include <math.h>
+#include <stdint.h>
+#include <stdlib.h>
 
-float nanf(const char* s)
+float _kernel_nanf(const char* s, char tail)
 {
-    return _kernel_nanf(s, '\0');
+    char* end;
+    uint32_t mantissa = isalnum(*s) * strtoul(s, &end, 0);
+    return reinterpret(float, 0x7FC00000 | (*end == tail) * mantissa);
 }
+
