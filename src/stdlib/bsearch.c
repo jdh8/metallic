29aa39e68be2880d21f5e7142854e3cfa734ac9b
Chen-Pang He <chen.pang.he@jdh8.org>
2018-10-20 04:12:23 +0800

    Implement bsearch

---
+++ src/stdlib/bsearch.c
@@ -0,0 +1,24 @@
+/* This file is part of Metallic, a runtime library for WebAssembly.
+ *
+ * Copyright (C) 2018 Chen-Pang He <chen.pang.he@jdh8.org>
+ *
+ * This Source Code Form is subject to the terms of the Mozilla
+ * Public License v. 2.0. If a copy of the MPL was not distributed
+ * with this file, You can obtain one at http://mozilla.org/MPL/2.0/
+ */
+#include <stddef.h>
+
+void* bsearch(const void* key, const void* begin, size_t count, size_t size, int compare(const void*, const void*))
+{
+    size_t half = count >> 1;
+    const void* pivot = (const char*)begin + size * half;
+    int comparison = compare(key, pivot);
+
+    if (comparison < 0)
+        return bsearch(key, begin, half, size, compare);
+
+    if (comparison)
+        return bsearch(key, pivot, count - half, size, compare);
+
+    return (void*)pivot;
+}
