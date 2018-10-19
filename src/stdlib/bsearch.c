f684f9406cc813b94b30b5d2a87e0010c0b2eeef
Chen-Pang He <chen.pang.he@jdh8.org>
2018-10-20 07:04:13 +0800

    Fix the not-found case of bsearch

--- src/stdlib/bsearch.c
+++ src/stdlib/bsearch.c
@@ -10,6 +10,9 @@
 
 void* bsearch(const void* key, const void* begin, size_t count, size_t size, int compare(const void*, const void*))
 {
+    if (!count)
+        return (void*)0;
+
     size_t half = count >> 1;
     const void* pivot = (const char*)begin + size * half;
     int comparison = compare(key, pivot);
