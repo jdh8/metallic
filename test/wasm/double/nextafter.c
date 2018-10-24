8a5af31f995c20851644603d61dd5e6d3a9461d2
Chen-Pang He <chen.pang.he@jdh8.org>
2018-10-24 14:47:59 +0800

    Test nextafter

---
+++ test/wasm/double/nextafter.c
@@ -0,0 +1,43 @@
+
+/* This file is part of Metallic, a runtime library for WebAssembly.
+ *
+ * Copyright (C) 2018 Chen-Pang He <chen.pang.he@jdh8.org>
+ *
+ * This Source Code Form is subject to the terms of the Mozilla
+ * Public License v. 2.0. If a copy of the MPL was not distributed
+ * with this file, You can obtain one at http://mozilla.org/MPL/2.0/
+ */
+#include "../assert.h"
+#include "identical.h"
+#include <math.h>
+
+static _Bool run(double from, double to)
+{
+    double x = from ? from : copysignf(from, to);
+    double y = nextafter(from, to);
+    _Bool near = reinterpret(uint64_t, x) - reinterpret(uint64_t, y) + 1 <= 2;
+
+    if (from == to)
+        return identical(to, y);
+
+    if (from < to)
+        return from < y && near;
+
+    if (from > to)
+        return from > y && near;
+
+    return isnan(y);
+}
+
+int main(void)
+{
+    for (uint64_t j = 0; j < 0x8000000000000000; j += 0x0003F46E86FBE2AA)
+        for (uint64_t i = 0; i < 0x8000000000000000; i += 0x00033158A6F1D0C4)
+            metallic_assert(run(reinterpret(double, i), reinterpret(double, j)));
+
+    for (uint64_t i = 0; i < 0x8000000000000000; i += 0x00033158A6F1D0C4)
+        metallic_assert(run(reinterpret(double, i), INFINITY));
+
+    for (uint64_t j = 0; j < 0x8000000000000000; j += 0x0003F46E86FBE2AA)
+        metallic_assert(run(INFINITY, reinterpret(double, j)));
+}
