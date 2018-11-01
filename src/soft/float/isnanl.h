3b7b9f99c3982326c0ac65417f44e8e12f4e7315
Chen-Pang He <chen.pang.he@jdh8.org>
2018-11-01 11:26:47 +0800

    Refactor __unordtf2

--- src/soft/float/isnanl.h
+++ src/soft/float/isnanl.h
@@ -6,20 +6,11 @@
  * Public License v. 2.0. If a copy of the MPL was not distributed
  * with this file, You can obtain one at http://mozilla.org/MPL/2.0/
  */
-#ifndef METALLIC_ISNANQ_H
-#define METALLIC_ISNANQ_H
-
+#include "../../math/reinterpret.h"
 #include <math.h>
 #include <stdint.h>
 
 static int __isnanl(long double x)
 {
-#ifdef __FAST_MATH__
-    return 0;
-#else
-    x = fabsl(x);
-    return *(unsigned __int128*)&x > (unsigned __int128) 0x7FFF << 112;
-#endif
+    return reinterpret(unsigned __int128, fabsl(x)) > (unsigned __int128)0x7FFF << 112;
 }
-
-#endif /* isnanl.h */
--- src/soft/float/unordtf2.h
+++ src/soft/float/unordtf2.h
@@ -6,14 +6,9 @@
  * Public License v. 2.0. If a copy of the MPL was not distributed
  * with this file, You can obtain one at http://mozilla.org/MPL/2.0/
  */
-#ifndef METALLIC_UNORDTF2_H
-#define METALLIC_UNORDTF2_H
-
 #include "isnanl.h"
 
 static int unordtf2(long double x, long double y)
 {
     return __isnanl(x) || __isnanl(y);
 }
-
-#endif
