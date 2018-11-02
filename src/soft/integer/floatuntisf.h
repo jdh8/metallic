3bdb927e154bcebb1d25c33a98d9c5e065c21168
Chen-Pang He <chen.pang.he@jdh8.org>
2018-11-02 12:18:44 +0800

    Implement unsigned __int128 -> float

---
+++ src/soft/integer/floatuntisf.h
@@ -0,0 +1,28 @@
+/* This file is part of Metallic, a runtime library for WebAssembly.
+ *
+ * Copyright (C) 2018 Chen-Pang He <chen.pang.he@jdh8.org>
+ *
+ * This Source Code Form is subject to the terms of the Mozilla
+ * Public License v. 2.0. If a copy of the MPL was not distributed
+ * with this file, You can obtain one at http://mozilla.org/MPL/2.0/
+ */
+#include "clzti2.h"
+#include "../../math/reinterpret.h"
+#include <stdint.h>
+
+static float _floatuntisf(unsigned __int128 a)
+{
+    const unsigned __int128 bit = (unsigned __int128)1 << 103;
+    const unsigned __int128 mask = 3 * bit - 1;
+
+    if (!a)
+        return 0;
+
+    int space = _clzti2(a);
+    unsigned __int128 normalized = a << space;
+    uint32_t adjustment = normalized & bit && normalized & mask;
+    uint32_t significand = (uint32_t)(normalized >> 104) & 0x7FFFFF;
+    uint32_t exp = 127 + 127 - space;
+
+    return reinterpret(float, (significand | exp << 23) + adjustment);
+}
---
+++ test/native/float/floatuntisf.c
@@ -0,0 +1,22 @@
+/* This file is part of Metallic, a runtime library for WebAssembly.
+ *
+ * Copyright (C) 2018 Chen-Pang He <chen.pang.he@jdh8.org>
+ *
+ * This Source Code Form is subject to the terms of the Mozilla
+ * Public License v. 2.0. If a copy of the MPL was not distributed
+ * with this file, You can obtain one at http://mozilla.org/MPL/2.0/
+ */
+#include "../../../src/soft/integer/floatuntisf.h"
+#include "unary.h"
+#include <stdint.h>
+
+int main(void)
+{
+    unsigned __int128 a = 0;
+
+    for (int_least32_t i = 0; i < INT32_C(1) << 26; ++i) {
+        verify(identical(_floatuntisf(a), a), a);
+        a = a + 1993 + (a >> 3);
+    }
+}
+
