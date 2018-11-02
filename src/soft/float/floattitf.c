4026d91b5a77433d7d4cd866e3966f57829aca50
Chen-Pang He <chen.pang.he@jdh8.org>
2018-11-02 12:22:05 +0800

    Adjust last to avoid overflow

--- src/soft/float/floattitf.c
+++ src/soft/float/floattitf.c
@@ -22,8 +22,8 @@ long double __floattitf(__int128 a)
     int space = _clzti2(magnitude);
     unsigned __int128 normalized = magnitude << space;
     unsigned __int128 adjustment = normalized & 0x4000 && normalized & 0xBFFF;
-    unsigned __int128 significand = (normalized >> 15 & mask) + adjustment;
+    unsigned __int128 significand = normalized >> 15 & mask;
     unsigned __int128 exp = 0x3FFF + 127 - space;
 
-    return reinterpret(long double, significand | exp << 112 | sign);
+    return reinterpret(long double, (significand | exp << 112 | sign) + adjustment);
 }
--- src/soft/float/floatuntitf.c
+++ src/soft/float/floatuntitf.c
@@ -19,8 +19,8 @@ long double __floatuntitf(unsigned __int128 a)
     int space = _clzti2(a);
     unsigned __int128 normalized = a << space;
     unsigned __int128 adjustment = normalized & 0x4000 && normalized & 0xBFFF;
-    unsigned __int128 significand = (normalized >> 15 & mask) + adjustment;
+    unsigned __int128 significand = normalized >> 15 & mask;
     unsigned __int128 exp = 0x3FFF + 127 - space;
 
-    return reinterpret(long double, significand | exp << 112);
+    return reinterpret(long double, (significand | exp << 112) + adjustment);
 }
