168966ff8a8209e0075ce1c4d68fa210dba2734e
Chen-Pang He <chen.pang.he@jdh8.org>
2018-11-07 01:12:16 +0800

    Fix a typo

--- include/stdarg.h
+++ include/stdarg.h
@@ -13,7 +13,7 @@ typedef __builtin_va_list va_list;
 
 #define va_start(list, antecedent) __builtin_va_start(list, antecedent)
 #define va_arg(list, type) __builtin_va_arg(list, type)
-#define va_end(list) __builtin_va_end(v)
+#define va_end(list) __builtin_va_end(list)
 
 #if __STDC_VERSION__ >= 199901L || __cplusplus >= 201103L
 #define va_copy(destination, source) __builtin_va_copy(destination, source)
