ed8b9c536a8675b84f778176857f8dc53ab89124
Chen-Pang He <chen.pang.he@jdh8.org>
2018-11-07 17:32:09 +0800

    Declare stdin, stdout, stderr

--- include/stdio.h
+++ include/stdio.h
@@ -83,9 +83,17 @@ int rename(const char[_STATIC 1], const char[_STATIC 1]);
 FILE* tmpfile(void);
 char* tmpnam(char*);
 
+extern FILE* stdin;
+extern FILE* stdout;
+extern FILE* stderr;
+
 #ifdef __cplusplus
 }
 #endif
+
+#define stdin stdin
+#define stdout stdout
+#define stderr stderr
 
 #define EOF -1
 #define FOPEN_MAX 16
