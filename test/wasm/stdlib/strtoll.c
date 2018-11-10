4e700abcecdf7338cd5d1d68ce016f5caa0a885d
Chen-Pang He <chen.pang.he@jdh8.org>
2018-11-11 03:25:10 +0800

    Test parsing 64-bit integers

---
+++ test/wasm/stdlib/strtoll.c
@@ -0,0 +1,60 @@
+/* This file is part of Metallic, a runtime library for WebAssembly.
+ *
+ * Copyright (C) 2018 Chen-Pang He <chen.pang.he@jdh8.org>
+ *
+ * This Source Code Form is subject to the terms of the Mozilla
+ * Public License v. 2.0. If a copy of the MPL was not distributed
+ * with this file, You can obtain one at http://mozilla.org/MPL/2.0/
+ */
+#include "../assert.h"
+#include <stddef.h>
+#include <stdint.h>
+#include <stdlib.h>
+#include <limits.h>
+#include <errno.h>
+
+#define RUN(s, junk, base) check(s junk, base, sizeof(s) - 1)
+#define CANONICAL(x, junk) metallic_assert(RUN(#x, junk, 0) == x)
+
+#define OVERFLOW(condition) do {      \
+    errno = 0;                        \
+    metallic_assert(condition);       \
+    metallic_assert(errno == ERANGE); \
+} while (0)
+
+static long long check(const char s[static 1], int base, ptrdiff_t length)
+{
+    char* end;
+    long long x = strtoll(s, &end, base);
+    metallic_assert(end == s + length);
+    return x;
+}
+
+int main(void)
+{
+    CANONICAL( +314159265358979, "$foo");
+    CANONICAL(  0x2018deadbeef1337, "gf");
+    CANONICAL(   -0232776532567256776, "9");
+
+    CANONICAL(9223372036854775807, "junk");
+    CANONICAL(-9223372036854775807, "junk");
+    CANONICAL(0, "x");
+
+    metallic_assert(RUN("", "owm111", 0) == 0);
+    metallic_assert(RUN("", "owm111", 24) == 0);
+    metallic_assert(RUN("ChenPangHe", "#1993", 30) == 247673263235924);
+    metallic_assert(RUN("ChenPangHe", "", 32) == 441410745254446);
+    metallic_assert(RUN("+ChenPangHe", "zzzzz", 34) == 759734417156424);
+    metallic_assert(RUN("-ChenPangHe", "", 36) == -1267827042283826);
+
+    metallic_assert(RUN("64ie1focnn5g77", "www", 25) == INT64_MAX);
+    metallic_assert(RUN("-64ie1focnn5g78", "www", 25) == INT64_MIN);
+
+    metallic_assert(RUN("1y2p0ij32e8e7", "...", 36) == INT64_MAX);
+    metallic_assert(RUN("-1y2p0ij32e8e8", "...", 36) == INT64_MIN);
+
+    metallic_assert(!errno);
+
+    OVERFLOW(RUN("-321f5a16ga5s1g65as05vs", "", 36) == LLONG_MIN);
+    OVERFLOW(RUN("154165asjndiniasndi3sd", "", 36) == LLONG_MAX);
+}
--- test/wasm/stdlib/strtoull.c
+++ test/wasm/stdlib/strtoull.c
@@ -20,30 +20,30 @@
     metallic_assert(errno == ERANGE);        \
 } while (0)
 
-static unsigned long check(const char s[static 1], int base, ptrdiff_t length)
+static unsigned long long check(const char s[static 1], int base, ptrdiff_t length)
 {
     char* end;
-    unsigned long x = strtoul(s, &end, base);
+    unsigned long long x = strtoull(s, &end, base);
     metallic_assert(end == s + length);
     return x;
 }
 
 int main(void)
 {
-    CANONICAL( +24680, "$foo");
-    CANONICAL(  0xdeadbeef, "gf");
-    CANONICAL(   -0232776532, "9");
+    CANONICAL( +314159265358979, "$foo");
+    CANONICAL(  0xdeadbeef13372018, "gf");
+    CANONICAL(   -0232776532567256776, "9");
     CANONICAL(0, "x");
 
-    metallic_assert(RUN("", "jdh8", 0) == 0);
-    metallic_assert(RUN("", "jdh8", 19) == 0);
-    metallic_assert(RUN("jdh8", "#1993", 20) == 157548);
-    metallic_assert(RUN("\tjdh8", "", 25) == 305433);
-    metallic_assert(RUN("\n+jdh8", "whatever", 30) == 525218);
-    metallic_assert(RUN(" -jdh8", "", 36) == -903932);
+    metallic_assert(RUN("", "owm111", 0) == 0);
+    metallic_assert(RUN("", "owm111", 24) == 0);
+    metallic_assert(RUN("ChenPangHe", "#1993", 30) == 247673263235924);
+    metallic_assert(RUN("ChenPangHe", "", 32) == 441410745254446);
+    metallic_assert(RUN("+ChenPangHe", "zzzzz", 34) == 759734417156424);
+    metallic_assert(RUN("-ChenPangHe", "", 36) == -1267827042283826);
 
-    metallic_assert(RUN("3vvvvvv", "...", 32) == -1);
-    metallic_assert(RUN("-3vvvvvv", "...", 32) == 1);
+    metallic_assert(RUN("c9c336o0mlb7ef", "...", 25) == -1);
+    metallic_assert(RUN("-c9c336o0mlb7ef", "...", 25) == 1);
 
     metallic_assert(!errno);
 
