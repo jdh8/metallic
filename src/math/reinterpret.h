32939d4e9a6574fb547dd5ce8d05bd4f995a1650
Chen-Pang He <chen.pang.he@jdh8.org>
2018-12-02 17:12:29 +0800

    Enforce equal size in reinterpret

--- src/math/reinterpret.h
+++ src/math/reinterpret.h
@@ -6,4 +6,8 @@
  * Public License v. 2.0. If a copy of the MPL was not distributed
  * with this file, You can obtain one at http://mozilla.org/MPL/2.0/
  */
-#define reinterpret(T, x) ((union { __typeof__(x) _from; T _to; }){ x }._to)
+#define reinterpret(T, x) ((union {               \
+    __typeof__(x) _from;                          \
+    T _to;                                        \
+    char _check[sizeof(T) == sizeof(x) ? 1 : -1]; \
+}){ x }._to)
