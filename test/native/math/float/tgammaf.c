b6b3a827748faf803a0a578b35b88149e807e966
Chen-Pang He <chen.pang.he@jdh8.org>
2018-12-26 00:04:56 +0800

    Test tgamma in [0.5, inf)

---
+++ test/native/math/float/tgammaf.c
@@ -0,0 +1,18 @@
+/* This file is part of Metallic, a runtime library for WebAssembly.
+ *
+ * Copyright (C) 2018 Chen-Pang He <chen.pang.he@jdh8.org>
+ *
+ * This Source Code Form is subject to the terms of the Mozilla
+ * Public License v. 2.0. If a copy of the MPL was not distributed
+ * with this file, You can obtain one at http://mozilla.org/MPL/2.0/
+ */
+#include "src/math/float/tgammaf.c"
+#include "unary.h"
+
+int main(void)
+{
+    for (uint32_t i = 0x3F000000; i < 0x7F800000; i += 137) {
+        float x = reinterpret(float, i);
+        verify(approx(tgammaf(x), tgamma(x), 1), x);
+    }
+}
