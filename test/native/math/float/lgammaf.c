a8e469db6553158f4de9ae2154b030563e4ede01
Chen-Pang He <chen.pang.he@jdh8.org>
2018-12-30 15:26:11 +0800

    Test lgammaf

---
+++ test/native/math/float/lgammaf.c
@@ -0,0 +1,18 @@
+/* This file is part of Metallic, a runtime library for WebAssembly.
+ *
+ * Copyright (C) 2018 Chen-Pang He <chen.pang.he@jdh8.org>
+ *
+ * This Source Code Form is subject to the terms of the Mozilla
+ * Public License v. 2.0. If a copy of the MPL was not distributed
+ * with this file, You can obtain one at http://mozilla.org/MPL/2.0/
+ */
+#include "src/math/float/lgammaf.c"
+#include "unary.h"
+
+int main(void)
+{
+    for (uint32_t i = 0; i <= 0x7F800000; i += 128) {
+        float x = reinterpret(float, i);
+        verify(approx(lgammaf(x), lgamma(x), 1), x);
+    }
+}
