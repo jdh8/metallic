3574b433a2d96464b240c179ecf9bcc907c25ec3
Chen-Pang He <chen.pang.he@jdh8.org>
2019-04-03 09:29:47 +0800

    Dismantle pointless tail call

--- src/math/double/log1p.c
+++ src/math/double/log1p.c
@@ -3,21 +3,6 @@
 #include <math.h>
 #include <stdint.h>
 
-static double _finite(double x)
-{
-    const double ln2[] = { 0x1.62e42fefa4p-1, -0x1.8432a1b0e2634p-43 };
-
-    double u = 1 + x;
-    int64_t i = reinterpret(int64_t, u);
-    int64_t exponent = (i - 0x3FE6A09E667F3BCD) >> 52;
-
-    double y = reinterpret(double, i - (exponent << 52)) - 1;
-    double z = y / (y + 2);
-    double h = 0.5 * y * y;
-
-    return exponent * ln2[1] + (1 - u + x) / u + z * (h + _kernel_log(z)) - h + y + exponent * ln2[0];
-}
-
 double log1p(double x)
 {
     int64_t i = reinterpret(int64_t, x);
@@ -31,5 +16,15 @@ double log1p(double x)
     if (i >= 0x7FF0000000000000 || (uint64_t)i << 1 < 0x7940000000000000)
         return x;
 
-    return _finite(x);
+    const double ln2[] = { 0x1.62e42fefa4p-1, -0x1.8432a1b0e2634p-43 };
+
+    double u = 1 + x;
+    int64_t j = reinterpret(int64_t, u);
+    int64_t exponent = (j - 0x3FE6A09E667F3BCD) >> 52;
+
+    double y = reinterpret(double, j - (exponent << 52)) - 1;
+    double z = y / (y + 2);
+    double h = 0.5 * y * y;
+
+    return exponent * ln2[1] + (1 - u + x) / u + z * (h + _kernel_log(z)) - h + y + exponent * ln2[0];
 }
