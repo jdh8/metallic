a3d608f256e7762da3dabc625b0ebf9bc790e2d0
Chen-Pang He <chen.pang.he@jdh8.org>
2019-04-03 09:12:38 +0800

    Save a branch in log1p

--- src/math/double/log1p.c
+++ src/math/double/log1p.c
@@ -8,16 +8,14 @@ static double _finite(double x)
     const double ln2[] = { 0x1.62e42fefa4p-1, -0x1.8432a1b0e2634p-43 };
 
     double u = 1 + x;
-
     int64_t i = reinterpret(int64_t, u);
     int64_t exponent = (i - 0x3FE6A09E667F3BCD) >> 52;
 
-    double c = exponent < 2 ? 1 - u + x : x - u + 1;
     double y = reinterpret(double, i - (exponent << 52)) - 1;
     double z = y / (y + 2);
     double h = 0.5 * y * y;
 
-    return exponent * ln2[1] + c / u + z * (h + _kernel_loga(z)) - h + y + exponent * ln2[0];
+    return exponent * ln2[1] + (1 - u + x) / u + z * (h + _kernel_loga(z)) - h + y + exponent * ln2[0];
 }
 
 double log1p(double x)
