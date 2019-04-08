fee7779522a9398d1ab78d89e181724e0ad4a46d
Chen-Pang He <chen.pang.he@jdh8.org>
2019-04-08 20:54:58 +0800

    Harden tests of log and log2

--- test/native/math/double/log.c
+++ test/native/math/double/log.c
@@ -5,8 +5,8 @@
 
 int main(void)
 {
-    for (int64_t i = 0; i < 0x7FF0000000000000; i += 0x000000888D16E29B) {
+    for (uint64_t i = 0; i < 0x7FF0000000000000; i += 0x49846664) {
         double x = reinterpret(double, i);
-        verify(approx(log(x), logl(x), 1), x);
+        verify(faithful(log(x), logl(x)), x);
     }
 }
