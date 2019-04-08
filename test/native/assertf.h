e76bb4340e2583baff557dc6992b13374ffbc1d3
Chen-Pang He <chen.pang.he@jdh8.org>
2019-04-08 18:33:52 +0800

    Guard ASSERTF with do ... while (0)

--- test/native/assertf.h
+++ test/native/assertf.h
@@ -3,10 +3,10 @@ _Noreturn void abort(void);
 #define STRINGIFY0(x) #x
 #define STRINGIFY(x) STRINGIFY0(x)
 
-#define ASSERTF(cond, ...) if (!(cond)) {              \
+#define ASSERTF(cond, ...) do if (!(cond)) {           \
     fputs(__FILE__":"STRINGIFY(__LINE__)": ", stderr); \
     fputs(__func__, stderr);                           \
     fputs(": Assertion `"#cond"' failed: ", stderr);   \
     fprintf(stderr, __VA_ARGS__);                      \
     abort();                                           \
-}
+} while (0)
