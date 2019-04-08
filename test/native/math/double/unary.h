89bcce93803c0153bc0f8c75537653cbfdfe028c
Chen-Pang He <chen.pang.he@jdh8.org>
2019-04-08 20:49:32 +0800

    Clear padding bits of long double in bit twiddling

--- test/native/math/double/unary.h
+++ test/native/math/double/unary.h
@@ -20,17 +20,26 @@ static inline _Bool approx(double x, double y, uint64_t tolerance)
     return a - b + tolerance <= 2 * tolerance;
 }
 
+#ifdef __SIZEOF_INT128__
+static inline unsigned __int128 _bitcast(long double x)
+{
+    if (LDBL_MANT_DIG > 64)
+        return reinterpret(unsigned __int128, x);
+
+    return (union {
+        long double from;
+        unsigned __int128 to: 10;
+    }){ x }.to;
+}
+#endif
+
 static inline _Bool faithful(long double x, long double y)
 {
     #if LDBL_MANT_DIG == DBL_MANT_DIG
         return approx(x, y, 1);
     #elif __SIZEOF_LONG_DOUBLE__ == __SIZEOF_INT128__
         const uint64_t mask = ((uint64_t)1 << (LDBL_MANT_DIG - DBL_MANT_DIG)) - 1;
-
-        unsigned __int128 a = reinterpret(unsigned __int128, x);
-        unsigned __int128 b = reinterpret(unsigned __int128, y);
-
-        return a - b + mask <= 2 * mask;
+        return _bitcast(x) - _bitcast(y) + mask <= 2 * mask;
     #else
         return identical(x, y) || (x && fabsl(x - y) <= fabs(nexttoward(x, y) - nexttoward(y, x)));
     #endif
