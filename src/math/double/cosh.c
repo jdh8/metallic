49ed9f3d704a20db6a45b2a0d65a69e9462a9b86
Chen-Pang He <chen.pang.he@jdh8.org>
2019-04-14 04:41:21 +0800

    Test borders of cosh

--- src/math/double/cosh.c
+++ src/math/double/cosh.c
@@ -3,36 +3,34 @@
 #include <math.h>
 #include <float.h>
 
-/* Restriction of (cosh ∘ √) to [0, (ln 2)^2] */
+/* Restriction of (cosh ∘ √) to [0, (0.5 ln 2)^2] */
 static double _kernel(double x)
 {
     const double c[] = {
-        1.0000000000000000083e-0,
-        4.9999999999999829907e-1,
-        4.1666666666723293644e-2,
-        1.3888888881819757406e-3,
-        2.4801591503375393704e-5,
-        2.7556037535355798775e-7,
-        2.1070483998613861298e-9
+        9.99999999999999996932e-1,
+        5.00000000000001838974e-1,
+        4.16666666664880674341e-2,
+        1.38888889523213161709e-3,
+        2.48014854761043149319e-5,
+        2.76326417118194562359e-7
     };
 
     double xx = x * x;
 
-    return (c[6] * xx + c[5] * x + c[4]) * (xx * xx) + (c[3] * x + c[2]) * xx + c[1] * x + c[0];
+    return (c[5] * x + c[4]) * (xx * xx) + (c[3] * x + c[2]) * xx + c[1] * x + c[0];
 }
 
 double cosh(double x)
 {
-    const double maximum = 710.475860073944;
     const double log2e = 1.44269504088896340736;
     const double ln2[] = { 0x1.62e42fefa4p-1, -0x1.8432a1b0e2634p-43 };
 
     x = fabs(x);
 
-    if (x > maximum)
-        return maximum * DBL_MAX;
+    if (reinterpret(uint64_t, x) > 0x408633CE8FB9F87D)
+        return x * HUGE_VAL;
 
-    if (x < ln2[0] + ln2[1])
+    if (x < 0.5 * (ln2[0] + ln2[1]))
         return _kernel(x * x);
 
     double n = rint(x * log2e);
--- test/native/math/double/cosh.c
+++ test/native/math/double/cosh.c
@@ -5,10 +5,21 @@
 
 int main(void)
 {
-    for (int64_t i = 0; i < 0x7FF0000000000000; i += 0x000000292482AD38) {
+    const uint64_t step = 0x000000292482AD38;
+
+    assert(cosh(INFINITY) == INFINITY);
+    assert(cosh(-INFINITY) == INFINITY);
+
+    for (uint64_t i = 0; i < 0x7FF0000000000000; i += step) {
         double x = reinterpret(double, i);
         double y = cosh(x);
         verify(faithful(y, coshl(x)), x);
         verify(y == cosh(-x), x);
+    }
+
+    for (uint64_t i = 0x7FF8000000000000; i < 0x8000000000000000; i += step) {
+        double x = reinterpret(double, i);
+        verify(isnan(cosh(x)), i);
+        verify(isnan(cosh(-x)), i);
     }
 }
