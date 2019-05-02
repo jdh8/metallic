abd2f3d0dc70fe554e4cae5a5c8cd1c99b5c7ee7
Chen-Pang He <chen.pang.he@jdh8.org>
2019-05-02 22:39:55 +0800

    Imperfect log10

---
+++ src/math/double/log10.c
@@ -0,0 +1,39 @@
+#include "kernel/log.h"
+#include "normalize.h"
+#include "../reinterpret.h"
+#include <math.h>
+
+static double _finite(int64_t i)
+{
+    const double log10_2[] = { 0x1.34413509f78p-2, 0x1.fef311f12b358p-46 };
+    const double log10_e[] = { 0x1.bcb7b152p-2, 0x1.b9438ca9aadd5p-36 };
+
+    int64_t exponent = (i - 0x3FE6A09E667F3BCD) >> 52;
+    double x = reinterpret(double, i - (exponent << 52)) - 1;
+    double z = x / (x + 2);
+    double h = 0.5 * x * x;
+    
+    uint64_t j = reinterpret(uint64_t, x - h) & 0xFFFFFFFF00000000;
+    double a = reinterpret(double, j);
+    double b = x - a - h + z * (h + _kernel_log(z));
+    double c = exponent * log10_2[1] + (a + b) * log10_e[1] + b * log10_e[0];
+    double u = exponent * log10_2[0];
+    double v = a * log10_e[0];
+    double s = u + v;
+    double e = u - s + v;
+
+    return c + e + s;
+}
+
+double log10(double x)
+{
+    int64_t i = reinterpret(int64_t, x);
+
+    if (i <= 0)
+        return i << 1 == 0 ? -HUGE_VAL : NAN;
+
+    if (i < 0x7FF0000000000000)
+        return _finite(_normalize(i));
+
+    return x;
+}
---
+++ test/native/math/double/log10.c
@@ -0,0 +1,7 @@
+#include "src/math/double/log2.c"
+#include "log.h"
+
+int main(void)
+{
+    test(log10, log10l);
+}
