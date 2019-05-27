1251e1c4455c7989a06c758fcc35114123a6d8e8
Chen-Pang He <chen.pang.he@jdh8.org>
2019-05-27 23:57:45 +0800

    Fix compatibility

--- src/stdio/stderr.c
+++ src/stdio/stderr.c
@@ -5,11 +5,11 @@ int __putcerr(int, FILE*);
 size_t __stderr(const void*, size_t, FILE*);
 
 static FILE _stderr = (FILE){
-    ._get = __invalid_get,
-    ._put = __putcerr,
-    ._unget = __invalid_put,
-    ._read = __invalid_read,
-    ._write = __stderr
+    .get = __invalid_get,
+    .put = __putcerr,
+    .unget = __invalid_put,
+    .read = __invalid_read,
+    .write = __stderr
 };
 
 FILE* stderr = &_stderr;
--- src/stdio/stdout.c
+++ src/stdio/stdout.c
@@ -5,11 +5,11 @@ int __putchar(int, FILE*);
 size_t __stdout(const void*, size_t, FILE*);
 
 static FILE _stdout = (FILE){
-    ._get = __invalid_get,
-    ._put = __putchar,
-    ._unget = __invalid_put,
-    ._read = __invalid_read,
-    ._write = __stdout
+    .get = __invalid_get,
+    .put = __putchar,
+    .unget = __invalid_put,
+    .read = __invalid_read,
+    .write = __stdout
 };
 
 FILE* stdout = &_stdout;
