3b9d4e227a23ef44e262e538179cb2590711c9dd
Chen-Pang He <chen.pang.he@jdh8.org>
2019-05-31 03:14:24 +0800

    Mark `lseek` as not implemented

--- test/wasm/syscalls.mjs
+++ test/wasm/syscalls.mjs
@@ -7,3 +7,5 @@ export const __setup = buffer => userspace = new Uint8Array(buffer);
 export const __read = (fd, pointer, size) => fs.readSync(fd, userspace, pointer, size);
 
 export const __write = (fd, pointer, size) => fs.writeSync(fd, userspace, pointer, size);
+
+export const __lseek = () => -38;
