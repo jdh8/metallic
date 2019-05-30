ba215571ffe7bfd4915b0a7326eb77c941ac0204
Chen-Pang He <chen.pang.he@jdh8.org>
2019-05-31 02:50:15 +0800

    Assume ptrdiff_t to be ssize_t

--- src/stdio/_write.c
+++ src/stdio/_write.c
@@ -2,18 +2,18 @@
 
 extern _Thread_local int errno;
 
-long __write(int, const void*, size_t);
+ptrdiff_t __write(int, const void*, size_t);
 
 size_t __stdio_write(const void* restrict buffer, size_t size, FILE stream[restrict static 1])
 {
-    long result = __write(stream->fd, buffer, size);
-    size_t count = result < 0 ? 0 : result;
+    ptrdiff_t status = __write(stream->fd, buffer, size);
+    size_t count = status < 0 ? 0 : status;
 
     if (count < size)
         stream->state |= _errbit;
 
-    if (result < 0)
-        errno = -result;
+    if (status < 0)
+        errno = -status;
 
     return count;
 }
