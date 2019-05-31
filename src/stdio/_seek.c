d2eaf8c9c88d44a1db95f04e4f4b7dfee5e61b45
Chen-Pang He <chen.pang.he@jdh8.org>
2019-05-31 14:29:22 +0800

    Tackle overflow in __stdio_seek

--- src/stdio/_seek.c
+++ src/stdio/_seek.c
@@ -1,16 +1,21 @@
 #include "FILE.h"
+#include <errno.h>
+#include <limits.h>
 #include <stdint.h>
 #include <stdio.h>
 
 typedef int_least64_t off_t;
-
-extern _Thread_local int errno;
 
 off_t __lseek(int, off_t, int);
 
 long __stdio_seek(FILE stream[restrict static 1], long offset, int origin)
 {
     off_t position = __lseek(stream->fd, offset, origin);
+
+    if (position > LONG_MAX) {
+        errno = EOVERFLOW;
+        return EOF;
+    }
 
     if (position < 0) {
         errno = -position;
