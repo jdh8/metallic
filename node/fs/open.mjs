a232a9123e7ac949be6f16990353dd7c2540e1cf
Chen-Pang He <chen.pang.he@jdh8.org>
2019-06-05 03:08:38 +0800

    Implement syscalls truncate and ftruncate

--- node/fs/open.mjs
+++ node/fs/open.mjs
@@ -6,3 +6,5 @@ import fs from "fs";
 export const __open = wrap((path, flags, mode) => fs.openSync(cstring(buffer, path), flags, mode));
 export const __close = wrap(fs.closeSync);
 export const __access = wrap((path, mode) => fs.accessSync(cstring(buffer, path), mode));
+export const __truncate = wrap((path, length) => fs.truncateSync(cstring(buffer, path), length));
+export const __ftruncate = wrap(fs.ftruncateSync);
