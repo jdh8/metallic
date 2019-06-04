f948d86bd05fbd9d9a2d63aae8ee8aadf425b68c
Chen-Pang He <chen.pang.he@jdh8.org>
2019-06-05 01:02:02 +0800

    Implement syscalls pread and pwrite

--- node/fs/read-write.mjs
+++ node/fs/read-write.mjs
@@ -1,7 +1,10 @@
 import { buffer } from "../memory.mjs";
+import wrap from "../internal/wrap.mjs";
 import fs from "fs";
 
-export const __read = (fd, pointer, size) => fs.readSync(fd, new Uint8Array(buffer, pointer, size));
-export const __write = (fd, pointer, size) => fs.writeSync(fd, new Uint8Array(buffer, pointer, size));
+export const __read = wrap((fd, ...rest) => fs.readSync(fd, new Uint8Array(buffer), ...rest));
+export const __write = wrap((fd, ...rest) => fs.writeSync(fd, new Uint8Array(buffer), ...rest));
 export const __lseek = () => -38;
 export const __llseek = () => -38;
+export const __pread = __read;
+export const __pwrite = __write;
