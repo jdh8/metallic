e4541dead4f1f2f96ffb0d4f241f574e9914260d
Chen-Pang He <chen.pang.he@jdh8.org>
2019-09-12 20:21:33 +0800

    Remove unused files

--- src/stdio/invalid.h
+++
@@ -1,8 +0,0 @@
-#include <stddef.h>
-
-typedef struct __FILE FILE;
-
-int __invalid_get(FILE*);
-int __invalid_put(int, FILE*);
-size_t __invalid_read(void*, size_t, FILE*);
-size_t __invalid_write(const void*, size_t, FILE*);
