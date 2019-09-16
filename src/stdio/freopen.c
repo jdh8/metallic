cde1d9209729c798fa0c5b615678ad1ab2a856a8
Chen-Pang He <chen.pang.he@jdh8.org>
2019-09-16 20:52:27 +0800

    Implement freopen despite of lacking __fcntl

---
+++ src/stdio/freopen.c
@@ -0,0 +1,40 @@
+#include "flush.h"
+#include "modeflags.h"
+#include "FILE_.h"
+#include <fcntl.h>
+
+extern _Thread_local int errno;
+
+int __open(const char[static 1], int, int);
+int __fcntl(int, int, unsigned long);
+
+FILE* freopen(const char path[restrict static 1], const char mode[restrict static 1], FILE stream[restrict static 1])
+{
+    int flags = modeflags_(mode);
+    flush_(stream);
+
+    if (path) {
+        stream->close(stream);
+        int fd = __open(path, flags, 0666);
+
+        if (fd >= 0) {
+            *stream = FILE_(fd);
+            return stream;
+        }
+
+        errno = -fd;
+    }
+    else {
+        if (flags & O_CLOEXEC)
+            __fcntl(stream->fd, F_SETFD, FD_CLOEXEC);
+
+        int status = __fcntl(stream->fd, F_SETFL, flags & ~(O_CREAT | O_EXCL | O_CLOEXEC));
+
+        if (status >= 0)
+            return stream;
+
+        errno = -status;
+    }
+
+    return (void*)0;
+}
