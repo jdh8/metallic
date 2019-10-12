c7e71ee6111ca152d16c317965ffb29a404ad4c4
Chen-Pang He <chen.pang.he@jdh8.org>
2019-10-12 21:15:10 +0800

    Test uint64_t -> long double

---
+++ test/wasm/soft/float/floatunditf.c
@@ -0,0 +1,21 @@
+#include "src/math/reinterpret.h"
+#include <stdint.h>
+#include <assert.h>
+
+uint64_t __fixunstfdi(long double);
+
+static _Bool run(uint64_t a)
+{
+    return a == __fixunstfdi(a);
+}
+
+int main(void)
+{
+    const uint64_t delta = 0x0008D46BA87B5A22;
+
+    for (uint64_t i = 0x3FFFFFFFFFFFFFFF; i < 0x4340000000000000; i += delta)
+        assert(run(reinterpret(double, i)));
+
+    for (uint64_t i = 0; i <= UINT64_MAX / delta; ++i)
+        assert(run(i * delta));
+}
