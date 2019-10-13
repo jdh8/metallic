dc8fefe43576fd91f59ccc3b59bbf3a437d7b6b5
Chen-Pang He <chen.pang.he@jdh8.org>
2019-10-13 15:05:03 +0800

    Optimize __fixunstfdi by ignoring undefined conversion

--- src/soft/float/fixunstfdi.c
+++ src/soft/float/fixunstfdi.c
@@ -7,14 +7,11 @@ uint64_t __fixunstfdi(long double x)
     int64_t high = bits >> 64;
     uint64_t low = bits;
 
-    if (high < 0x3FFF000000000000 || high >= 0x40AF000000000000)
+    if (high < 0x3FFF000000000000)
         return 0;
 
     int shift = 112 + 0x3FFF - (int)(high >> 48);
     uint64_t significand = (high & 0x0000FFFFFFFFFFFF) | 0x0001000000000000;
-
-    if (shift <= 0)
-        return low << -shift;
 
     if (shift < 64)
         return significand << (64 - shift) | low >> shift;
