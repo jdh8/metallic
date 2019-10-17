ed395b224909875b5da21ca2bc40091cbc033a5c
Chen-Pang He <chen.pang.he@jdh8.org>
2019-10-17 21:33:20 +0800

    Robustify test of uint64_t -> long double

--- test/wasm/soft/float/floatunditf.c
+++ test/wasm/soft/float/floatunditf.c
@@ -11,7 +11,7 @@ static _Bool run(uint64_t a)
 
 int main(void)
 {
-    const uint64_t delta = 0x0008D46BA87B5A22;
+    const uint64_t delta = 0x0000002BA87B5A22;
 
     for (uint64_t i = 0x3FFFFFFFFFFFFFFF; i < 0x4340000000000000; i += delta)
         assert(run(reinterpret(double, i)));
