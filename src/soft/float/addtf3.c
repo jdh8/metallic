950406e4a8bf17200936c20b4fab3187b91c3f65
Chen-Pang He <chen.pang.he@jdh8.org>
2019-10-31 16:21:37 +0800

    Special cases of soft float addition

--- src/soft/float/addtf3.c
+++ src/soft/float/addtf3.c
@@ -1,6 +1,31 @@
 #include "../../math/reinterpret.h"
 
-static unsigned __int128 sorted_(unsigned __int128, unsigned __int128);
+static __int128 add_(__int128 a, __int128 b);
+static __int128 sub_(__int128 a, __int128 b);
+
+static unsigned __int128 sorted_(unsigned __int128 a, unsigned __int128 b)
+{
+    const unsigned __int128 inf = (unsigned __int128)0x7FFF << 112;
+    const unsigned __int128 sign = (unsigned __int128)1 << 127;
+
+    unsigned __int128 aa = a & ~sign;
+    unsigned __int128 bb = b & ~sign;
+
+    if (aa > inf)
+        return a | (unsigned __int128)1 << 111;
+
+    if ((a ^ b) >> 127) {
+        if (aa == inf)
+            return a | (unsigned __int128)(bb == inf) << 111;
+
+        if (aa == bb)
+            return 0;
+
+        return (a & sign) | sub_(aa, bb);
+    }
+
+    return (a & sign) | add_(aa, bb);
+}
 
 long double __addtf3(long double x, long double y)
 {
