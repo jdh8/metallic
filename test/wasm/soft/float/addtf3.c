c24b5fcff1445c7360d2440b952de67fdd448ffb
Chen-Pang He <chen.pang.he@jdh8.org>
2019-11-09 17:44:56 +0800

    Test finite soft float addition

--- test/wasm/soft/float/addtf3.c
+++ test/wasm/soft/float/addtf3.c
@@ -18,4 +18,22 @@ int main(void)
 
     assert(run(INFINITY, INFINITY));
     assert(run(INFINITY, -0x1.23456789abcdefp+3849L));
+
+    assert(run(0x1.9930120p16382L,  0x1.9930901p16382L));
+    assert(run(0x1.9930120p16382L, -0x1.9930901p16382L));
+
+    assert(run(0x19930120p100L,  0x19930901p0L));
+    assert(run(0x19930120p100L, -0x19930901p0L));
+
+    assert(run(0.19930120L,  0.19930901L));
+    assert(run(0.19930120L, -0.19930901L));
+
+    assert(run(1.9930120e-4932L,  1.9930901e-4932L));
+    assert(run(1.9930120e-4932L, -1.9930901e-4932L));
+
+    assert(run(19930120,  0.0L));
+    assert(run(19930120, -0.0L));
+
+    assert(run(19930120, -19930120));
+    assert(run(0.0L, -0.0L));
 }
