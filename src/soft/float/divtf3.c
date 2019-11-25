fa917bdcacb4a16fc974212a1bd5e00497193af4
Chen-Pang He <chen.pang.he@jdh8.org>
2019-11-25 20:48:13 +0800

    Imprecise soft float division

--- src/soft/float/divtf3.c
+++ src/soft/float/divtf3.c
@@ -1,8 +1,31 @@
 #include "compose/product.h"
+#include "../integer/kernel/umulti.h"
 #include "../../math/long-double/normalizel.h"
 #include "../../math/reinterpret.h"
+#include <stdint.h>
 
-static unsigned __int128 fixdiv_(unsigned __int128 a, unsigned __int128 b);
+static uint64_t iterate_(uint64_t estimate, uint64_t denominator)
+{
+    uint64_t correction = umuldi_(estimate, denominator) >> 64;
+
+    return umuldi_(estimate, -correction) >> 63;
+}
+
+static unsigned __int128 fixdiv_(unsigned __int128 a, unsigned __int128 b)
+{
+    uint64_t estimate = 0x1p127 / (b >> 64);
+    estimate = iterate_(estimate, b >> 64);
+    estimate = iterate_(estimate, b >> 64);
+    --estimate;
+
+    unsigned __int128 correction = -(umuldi_(estimate, b >> 64) + ((estimate * b) >> 64));
+    unsigned __int128 reciprocal = (umuldi_(estimate, correction >> 64) << 1) + (umuldi_(estimate, correction) >> 63) - 2;
+    unsigned __int128 q[2];
+
+    umulti_(q, a, reciprocal);
+
+    return q[1] | !!q[0];
+}
 
 static unsigned __int128 kernel_(__int128 a, __int128 b)
 {
@@ -20,14 +43,14 @@ static unsigned __int128 magnitude_(unsigned __int128 a, unsigned __int128 b)
     if (a >= inf)
         return a | (unsigned __int128)(b >= inf) << 111;
 
+    if (!b)
+        return inf | (unsigned __int128)!a << 111;
+
     if (b > inf)
         return b | (unsigned __int128)1 << 111;
 
     if (!a || b == inf)
         return 0;
-
-    if (!b)
-        return inf | (unsigned __int128)!a << 111;
 
     return kernel_(normalizel_(a), normalizel_(b));
 }
