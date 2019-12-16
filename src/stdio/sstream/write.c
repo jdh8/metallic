4e86455da2b7a5b61c5c5bc565ecb67e6eebd7f2
Chen-Pang He <chen.pang.he@jdh8.org>
2019-12-16 23:40:09 +0800

    Move string stream code to implementation because it is used only once

--- src/stdio/sstream/write.c
+++
@@ -1,9 +0,0 @@
-#include <string.h>
-#include "../FILE.h"
-
-size_t __sstream_write(const void* restrict buffer, size_t size, FILE stream[restrict static 1])
-{
-    memcpy(stream->ptr, buffer, size);
-    stream->ptr += size;
-    return size;
-}
--- src/stdio/vsprintf.c
+++ src/stdio/vsprintf.c
@@ -1,12 +1,24 @@
-#include "sstream.h"
+#include "FILE.h"
 #include <stdio.h>
 #include <stdarg.h>
+#include <string.h>
+
+static size_t write_(const void* restrict buffer, size_t size, FILE stream[restrict static 1])
+{
+    memcpy(stream->ptr, buffer, size);
+    stream->ptr += size;
+    return size;
+}
 
 int vsprintf(char* s, const char format[restrict static 1], va_list list) 
 {
-    FILE stream = sstream_(s);
-    int status = vfprintf(&stream, format, list);
+    FILE stream = {
+        .base = (unsigned char*)s,
+        .ptr = (unsigned char*)s,
+        .write = write_,
+    };
 
+    int status = vfprintf(&stream, format, list);
     *stream.ptr = '\0';
     return status;
 }
