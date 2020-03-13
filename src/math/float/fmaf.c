63817ca0e4bdf8781a7d6d5283b115830e7bf272
Chen-Pang He <chen.pang.he@jdh8.org>
2020-03-13 16:29:25 +0800

    Make fmaf portable and fast

--- src/math/float/fmaf.c
+++ src/math/float/fmaf.c
@@ -5,6 +5,11 @@
 float fmaf(float a, float b, float c)
 {
     #pragma STDC FENV_ACCESS ON
+    #pragma STDC FP_CONTRACT ON
+
+    #ifdef FP_FAST_FMAF
+        return a * b + c;
+    #endif
 
     double ab = (double)a * b;
     double s = ab + c;
@@ -13,6 +18,12 @@ float fmaf(float a, float b, float c)
     if ((i & 0x1FFFFFFF) != 0x10000000 || s - s || s - ab == c || fegetround() != FE_TONEAREST)
         return s;
 
-    uint64_t adjustment = 1 - ((i ^ reinterpret(uint64_t, s - ab - c)) >> 63 << 1);
+    #ifdef FE_TOWARDZERO
+        uint64_t adjustment = !fesetround(FE_TOWARDZERO) && c + 0 + ab == s;
+        fesetround(FE_TONEAREST);
+    #else
+        uint64_t adjustment = 1 - ((i ^ reinterpret(uint64_t, s - ab - c)) >> 63 << 1);
+    #endif
+
     return reinterpret(double, i + adjustment);
 }
