95806378658591057ca2b27afda9ce585973c4fe
Chen-Pang He <chen.pang.he@jdh8.org>
2020-04-05 22:14:38 +0800

    Consistent coding style with SSA

--- src/math/double/sin.c
+++ src/math/double/sin.c
@@ -5,7 +5,6 @@ double sin(double x)
     double y[2];
     unsigned q = __rem_pio2(x, y);
 
-    x = q & 1 ? kernel_cos_(y[0], y[1]) : kernel_sin_(y[0], y[1]);
-
-    return q & 2 ? -x : x;
+    double z = q & 1 ? kernel_cos_(y[0], y[1]) : kernel_sin_(y[0], y[1]);
+    return q & 2 ? -z : z;
 }
