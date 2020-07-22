dc9e7d17a1458c53810828dbcbf13e1c5ec91e7a
Chen-Pang He <chen.pang.he@jdh8.org>
2020-07-23 01:47:00 +0800

    Optimize 128-bit integer division

--- src/soft/integer/udivmodti4.h
+++ src/soft/integer/udivmodti4.h
@@ -1,32 +1,54 @@
-#include "clzti2.h"
 #include <stdint.h>
 
-static unsigned __int128 shl_(unsigned __int128 x, int shift)
+static uint64_t divtri_(uint64_t a1, uint32_t a0, uint64_t b, uint64_t r[static 1])
 {
-    uint64_t high = x >> 64;
-    uint64_t low = x;
+    uint64_t b1 = b >> 32;
+    uint64_t b0 = b & 0xFFFFFFFF;
+    uint64_t q = a1 / b1;
 
-    if (shift)
-        return (unsigned __int128)(high << shift | low >> (64 - shift)) << 64 | low << shift;
+    for (uint64_t rr = a1 - q * b1; q > 0xFFFFFFFF || q * b0 > (rr << 32 | a0); ) {
+        --q;
+        rr += b1;
 
-    return x;
+        if (rr > 0xFFFFFFFF)
+            break;
+    }
+
+    *r = (a1 << 32 | a0) - q * b;
+    return q;
 }
 
-static uint64_t iterate_(unsigned __int128 a, unsigned __int128 b, unsigned __int128 r[static 1])
+static uint64_t divq_normalized_(unsigned __int128 a, uint64_t b, uint64_t r[static 1])
 {
-    uint64_t q = 0;
-    int shift = clzti2_(b) - clzti2_(a);
-    b = shl_(b, shift);
+    uint64_t q1 = divtri_(a >> 64, a >> 32, b, r);
+    uint64_t q0 = divtri_(*r, a, b, r);
 
-    for (uint64_t bit = (uint64_t)1 << shift; bit; bit >>= 1) {
-        if (a >= b) {
-            a -= b;
-            q |= bit;
-        }
-        b >>= 1;
-    }
-    *r = a;
+    return (q1 << 32) + q0;
+}
+
+static unsigned __int128 shl_(uint64_t high, uint64_t low, int shift)
+{
+    return shift
+        ? (unsigned __int128)(high << shift | low >> (64 - shift)) << 64 | low << shift
+        : (unsigned __int128)high << 64 | low;
+}
+
+static uint64_t divq_soft_(uint64_t a2, uint64_t a0, uint64_t b, uint64_t r[static 1])
+{
+    int s = __builtin_clzll(b);
+    uint64_t q = divq_normalized_(shl_(a2, a0, s), b << s, r);
+    *r >>= s;
     return q;
+}
+
+static uint64_t divq_(uint64_t a2, uint64_t a0, uint64_t b, uint64_t r[static 1])
+{
+#ifdef __x86_64__
+    uint64_t result;
+    __asm__("divq %[b]" : "=a"(result), "=d"(*r) : [b] "r"(b), "a"(a0), "d"(a2));
+    return result;
+#endif
+    return divq_soft_(a2, a0, b, r);
 }
 
 static unsigned __int128 udivmodti4_(unsigned __int128 a, unsigned __int128 b, unsigned __int128 r[static 1])
@@ -41,21 +63,25 @@ static unsigned __int128 udivmodti4_(unsigned __int128 a, unsigned __int128 b, u
         return 0;
     }
 
-    if (!a2) {
-        *r = a0 % b0;
-        return a0 / b0;
-    }
-
-    if (!b0) {
-        *r = (unsigned __int128)(a2 % b2) << 64 | a0;
-        return a2 / b2;
-    }
-
     if (!b2) {
+        uint64_t r0;
         uint64_t q2 = a2 / b0;
-        uint64_t q0 = iterate_((unsigned __int128)(a2 % b0) << 64 | a0, b0, r);
+        uint64_t q0 = divq_(a2 % b0, a0, b0, &r0);
+
+        *r = r0;
         return (unsigned __int128)q2 << 64 | q0;
     }
 
-    return iterate_(a, b, r);
+    uint64_t q = 0;
+    int shift = __builtin_clzll(b >> 64) - __builtin_clzll(a >> 64);
+
+    for (b = shl_(b2, b0, shift); shift >= 0; --shift) {
+        __int128 s = (__int128)(b - a - 1) >> 127;
+        q = (q << 1) | (s & 1);
+        a -= b & s;
+        b >>= 1;
+    }
+
+    *r = a;
+    return q;
 }
