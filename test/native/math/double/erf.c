270ebc87f4e0c9397c356719ac907bfd74de4629
Chen-Pang He <chen.pang.he@jdh8.org>
2020-08-16 04:05:25 +0800

    Complete test of erf

--- test/native/math/double/erf.c
+++ test/native/math/double/erf.c
@@ -5,10 +5,21 @@
 
 int main(void)
 {
-    const uint64_t step = 0x0000000636FC2447;
+    const uint64_t step = 0x00000004F2931D55;
+
+    assert(erf(HUGE_VAL) == 1);
+    assert(erf(-HUGE_VAL) == -1);
 
     for (uint64_t i = 0; i < 0x7FF0000000000000; i += step) {
         double x = reinterpret(double, i);
-        verify(approx(erf(x), erfl(x), 1), x);
+        double y = erf(x);
+        verify(approx(y, erfl(x), 1), x);
+        verify(identical(-y, erf(-x)), x);
+    }
+
+    for (uint64_t i = 0x7FF8000000000000; i < 0x8000000000000000; i += step) {
+        double x = reinterpret(double, i);
+        verify(isnan(erf(x)), x);
+        verify(isnan(erf(-x)), x);
     }
 }
